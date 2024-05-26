29782a6ecccecce0cb0b04443211a94b5296f2e6
andresgc04 <andresgc1997@outlook.com>
2024-05-26 14:47:03 -0400

    Agregando y diseniando vista del dashboard

--- view/templates/content-header.php
+++ view/templates/content-header.php
@@ -2,15 +2,15 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 class="m-0"><%= titleContentHeader %></h1>
+                <h1 class="m-0" id="titleContentHeader"></h1>
             </div>
             <!-- /.col -->
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item">
-                        <a href="<%= linkContentHeader %>"><%= titleBreadCrumbContentHeader %></a>
+                        <a id="titleBreadCrumbContentHeader"></a>
                     </li>
-                    <li class="breadcrumb-item active"><%= subtitleBreadCrumbContentHeader %></li>
+                    <li class="breadcrumb-item active" id="subTitleBreadCrumbContentHeader"></li>
                 </ol>
             </div>
             <!-- /.col -->
