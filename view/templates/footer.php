514d908c5a0283b1ccaaf3b5c0fe28aa8969aa59
andresgc04 <andresgc1997@outlook.com>
2024-05-26 13:43:02 -0400

    Agregando Los Componentes Del Template
    Agregando los siguientes componentes: content-header, control-sidebar, footer, head, main-sidebar-container, navbar, preloader y scripts.

---
+++ view/templates/footer.php
@@ -0,0 +1,8 @@
+<!-- Main Sidebar Container -->
+<footer class="main-footer">
+    <strong>Copyright &copy; 2024-2024
+        <a href="#">ProGestor</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block"><b>Version</b> 1.0.0</div>
+</footer>
+<!-- /.Main Sidebar Container -->
