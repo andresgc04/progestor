e25087597b34cc69734357a6fcc1068c4e0b5135
andresgc04 <andresgc1997@outlook.com>
2024-06-05 01:15:32 -0400

    Agregando Funcion De Listado De Paises En El Controlador De Paises
    Agregando Funcion De Listados De Paises En El Controlador De Paises y agregando funcionalidad para consumir el controlador y obtener los datos en el datatable.

--- controller/PaisesController.php
+++ controller/PaisesController.php
@@ -13,4 +13,31 @@ switch ($_GET["op"]) {
     case "registrar_pais":
         $paises->registrar_pais($_POST["nombrePais"], $creadoPor);
         break;
+    case "listado_paises":
+        $datos = $paises->listado_paises();
+        $data = array();
+
+        foreach ($datos as $row) {
+            $sub_array = array();
+            $sub_array[] = $row['PAIS_ID'];
+            $sub_array[] = $row['PAISES'];
+
+            if ($row["ESTADOS"] === "ACTIVO") {
+                $sub_array[] = '<span class="badge badge-primary">ACTIVO</span>';
+            }
+
+            $sub_array[] = '<button type="button" id="' . $row["PAIS_ID"] . '" class="btn btn-primary"><i class="fa fa-eye"></i></button>';
+
+            $data[] = $sub_array;
+        }
+
+        $resultados = array(
+            "sEcho" => 1,
+            "iTotalRecords" => count($data),
+            "iTotalDisplayRecords" => count($data),
+            "aaData" => $data
+        );
+
+        echo json_encode($resultados);
+        break;
 }
--- view/home-countries/home-countries.js
+++ view/home-countries/home-countries.js
@@ -16,3 +16,61 @@ const newCountryButton = document.getElementById("newCountryButton");
 newCountryButton.addEventListener("click", () => {
   openNewCountryFormModal();
 });
+
+const obtenerListadoPaisesDataTable = () => {
+  $("#listadoPaisesDataTable")
+    .dataTable({
+      aProcessing: true,
+      aServerSide: true,
+      dom: "Bfrtip",
+      searching: true,
+      lengthChange: false,
+      colReorder: true,
+      buttons: ["copyHtml5", "excelHtml5", "csvHtml5", "pdfHtml5"],
+      ajax: {
+        url: "../../controller/PaisesController.php?op=listado_paises",
+        type: "post",
+        dataType: "json",
+        error: function (e) {
+          console.log(e.responseText);
+        },
+      },
+      ordering: true,
+      bDestroy: true,
+      responsive: true,
+      bInfo: true,
+      iDisplayLength: 10,
+      autoWidth: false,
+      language: {
+        sProcessing: "Procesando...",
+        sLengthMenu: "Mostrar _MENU_ registros",
+        sZeroRecords: "No se encontraron resultados",
+        sEmptyTable: "Ningun dato disponible en esta tabla",
+        sInfo: "Mostrando un total de _TOTAL_ registros",
+        sInfoEmpty: "Mostrando un total de 0 registros",
+        sInfoFiltered: "(filtrado de un total de _MAX_ registros)",
+        sInfoPostFix: "",
+        sSearch: "Buscar:",
+        sUrl: "",
+        sInfoThousands: ",",
+        sLoadingRecords: "Cargando....",
+        oPaginate: {
+          sFirst: "Primero",
+          sLast: "Ultimo",
+          sNext: "Siguiente",
+          sPrevious: "Anterior",
+        },
+        oAria: {
+          sSortAscending:
+            ": Activar para ordenar la columna de manera ascendente",
+          sSortDescending:
+            ": Activar para ordenar la columna de manera descendente",
+        },
+      },
+    })
+    .DataTable();
+};
+
+(function () {
+  obtenerListadoPaisesDataTable();
+})();
