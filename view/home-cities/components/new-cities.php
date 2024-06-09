48b029bccd6758aca7a9eb6fa743027ffac49f9e
andresgc04 <andresgc1997@outlook.com>
2024-06-09 09:16:34 -0400

    Agregando Vista Del Formulario De Ciudades
    Agregando vista del formulario de ciudades para agregar nuevas ciudades.

---
+++ view/home-cities/components/new-cities.php
@@ -0,0 +1,47 @@
+<div class="modal fade" id="newCitiesFormModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Registrar Nueva Ciudad</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form id="newCitiesForm" name="newCitiesForm" method="post">
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                            <div class="form-group">
+                                <label for="paisID">Pa&iacute;s:</label>
+                                <select id="paisID" name="paisID" class="form-control select2" style="width: 100%;"></select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                            <div class="form-group">
+                                <label for="provinciaID">Provincia:</label>
+                                <select id="provinciaID" name="provinciaID" class="form-control select2" style="width: 100%;"></select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                            <div class="form-group">
+                                <label for="nombreCiudad">Ciudad:</label>
+                                <input type="text" id="nombreCiudad" name="nombreCiudad" class="form-control" placeholder="Ingrese el nombre de la ciudad.">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer justify-content-between">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal"><span class="mr-2">Cancelar</span><span><i class="fas fa-times-circle"></i></span></button>
+                    <button type="submit" class="btn btn-primary"><span class="mr-2">Guardar</span><span><i class="fas fa-save"></i></span></button>
+                </div>
+            </form>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
--- view/home-cities/home-cities.js
+++ view/home-cities/home-cities.js
@@ -7,3 +7,12 @@ setContentHeaderTitle("Listado De Ciudades");
 setBreadCrumbContentHeaderTitle("../dashboard/", "Dashboard");
 
 setBreadCrumbContentHeaderSubTitle("Listado De Ciudades");
+
+const openNewCitiesFormModal = () => {
+  $("#newCitiesFormModal").modal("show");
+};
+
+const newCityButton = document.getElementById("newCityButton");
+newCityButton.addEventListener("click", () => {
+  openNewCitiesFormModal();
+});
