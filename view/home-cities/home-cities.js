6bfbc1ebcc711c9ae7b5f62777255fcb67578d66
andresgc04 <andresgc1997@outlook.com>
2024-06-09 10:26:22 -0400

    Agregando funcionalidad para mostrar los listados de ciudades existentes

--- view/home-cities/home-cities.js
+++ view/home-cities/home-cities.js
@@ -16,3 +16,69 @@ const newCityButton = document.getElementById("newCityButton");
 newCityButton.addEventListener("click", () => {
   openNewCitiesFormModal();
 });
+
+const obtenerListadoCiudadesDataTable = () => {
+  $("#listadoCiudadesDataTable")
+    .dataTable({
+      aProcessing: true,
+      aServerSide: true,
+      dom: "Bfrtip",
+      searching: true,
+      lengthChange: false,
+      colReorder: true,
+      buttons: ["copyHtml5", "excelHtml5", "csvHtml5", "pdfHtml5"],
+      ajax: {
+        url: "../../controller/CiudadesController.php?op=listado_ciudades",
+        type: "post",
+        dataType: "json",
+        error: function (e) {
+          console.log(e.responseText);
+        },
+      },
+      ordering: false,
+      bDestroy: true,
+      responsive: true,
+      bInfo: true,
+      iDisplayLength: 10,
+      autoWidth: false,
+      language: {
+        sProcessing: "Procesando...",
+        sLengthMenu: "Mostrar _MENU_ registros",
+        sZeroRecords: "No se encontraron resultados",
+        sEmptyTable: "Ningun dato disponible en esta tabla",
+        sInfo: "Mostrando un total de _TOTAL_ registros",
+        sInfoEmpty: "Mostrando un total de 0 registros",
+        sInfoFiltered: "(filtrado de un total de _MAX_ registros)",
+        sInfoPostFix: "",
+        sSearch: "Buscar:",
+        sUrl: "",
+        sInfoThousands: ",",
+        sLoadingRecords: "Cargando....",
+        oPaginate: {
+          sFirst: "Primero",
+          sLast: "Ultimo",
+          sNext: "Siguiente",
+          sPrevious: "Anterior",
+        },
+        oAria: {
+          sSortAscending:
+            ": Activar para ordenar la columna de manera ascendente",
+          sSortDescending:
+            ": Activar para ordenar la columna de manera descendente",
+        },
+      },
+    })
+    .DataTable();
+};
+
+(function () {
+  //Initialize Select2 Elements:
+  initializeSelect2Elements();
+
+  obtenerListadoCiudadesDataTable();
+
+  getSelectListProvincesOptions(
+    "../../controller/PaisesController.php?op=obtener_listado_opciones_paises",
+    "#paisID"
+  );
+})();
