6cd9506969b0ac46ec2b87f609854dc4731611a9
andresgc04 <andresgc1997@outlook.com>
2024-06-09 21:48:07 -0400

    Creando y Maquetando Formulario De Registro De Tipos De Recursos Materiales

--- view/home-types-material-resources/home-types-material-resources.js
+++ view/home-types-material-resources/home-types-material-resources.js
@@ -7,3 +7,14 @@ setContentHeaderTitle("Listado De Tipos De Recursos Materiales");
 setBreadCrumbContentHeaderTitle("../dashboard/", "Dashboard");
 
 setBreadCrumbContentHeaderSubTitle("Listado De Tipos De Recursos Materiales");
+
+const openNewTypesMaterialResourcesFormModal = () => {
+  $("#newTypesMaterialResourcesFormModal").modal("show");
+};
+
+const newTypesMaterialResourcesButton = document.getElementById(
+  "newTypesMaterialResourcesButton"
+);
+newTypesMaterialResourcesButton.addEventListener("click", () => {
+  openNewTypesMaterialResourcesFormModal();
+});
