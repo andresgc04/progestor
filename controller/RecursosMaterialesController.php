914c93fd0626f601f143199cc560e48363709f31
andresgc04 <andresgc1997@outlook.com>
2024-06-11 23:13:00 -0400

    Creando el endpoint para consumir el listado de recursos materiales en el controlador de recursos materiales

--- controller/RecursosMaterialesController.php
+++ controller/RecursosMaterialesController.php
@@ -11,4 +11,38 @@ switch ($_GET['op']) {
     case 'registrar_recursos_materiales':
         $recursosMateriales->registrar_recursos_materiales($_POST['tipoRecursoMaterialID'], $_POST['nombreRecursoMaterial'], $creadoPor);
         break;
+    case 'listado_recursos_materiales':
+        $datos = $recursosMateriales->listado_recursos_materiales();
+        $data = array();
+
+        foreach ($datos as $row) {
+            $sub_array = array();
+            $sub_array[] = $row['RECURSO_MATERIAL_ID'];
+            $sub_array[] = $row['TIPOS_RECURSOS_MATERIALES'];
+            $sub_array[] = $row['RECURSOS_MATERIALES'];
+
+            if ($row['ESTADOS'] === 'ACTIVO') {
+                $sub_array[] = '<span class="badge badge-primary">ACTIVO</span>';
+            }
+
+            $sub_array[] = '<td class="text-right py-0 align-middle">
+                                <div class="btn-group btn-group-sm">
+                                    <button type="button" id="' . $row['RECURSO_MATERIAL_ID'] . '" onClick="verDetalleRecursoMaterial(' . $row['TIPO_RECURSO_MATERIAL_ID'] . ', ' . $row['RECURSO_MATERIAL_ID'] . ')" class="btn btn-info"><i class="fas fa-eye"></i></button>
+                                    <button type="button" id="' . $row['RECURSO_MATERIAL_ID'] . '" onClick="eliminarRecursoMaterial(' . $row['TIPO_RECURSO_MATERIAL_ID'] . ', ' . $row['RECURSO_MATERIAL_ID'] . ')" class="btn btn-danger"><i class="fas fa-trash"></i></button>
+                                </div>
+                            </td>
+                            ';
+
+            $data[] = $sub_array;
+        }
+
+        $resultados = array(
+            "sEcho" => 1,
+            "iTotalRecords" => count($data),
+            "iTotalDisplayRecords" => count($data),
+            "aaData" => $data
+        );
+
+        echo json_encode($resultados);
+        break;
 }
