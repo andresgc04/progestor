d7a495615e86a7cf40f92ee34a5fe724adffbaf0
andresgc04 <andresgc1997@outlook.com>
2024-06-11 22:06:49 -0400

    Agregando Funcionalidad Para Registrar Recursos Materiales
    Agregando funcionalidad para registrar nuevos recursos materiales en el modelo de recursos materiales

---
+++ models/RecursosMateriales.php
@@ -0,0 +1,21 @@
+<?php
+class RecursosMateriales extends Connection
+{
+    public function registrar_recursos_materiales($tipoRecursoMaterialID, $recursoMaterial, $creadoPor)
+    {
+        $conectar = parent::Connection();
+        parent::set_names();
+
+        $query = 'INSERT INTO RECURSOS_MATERIALES (TIPO_RECURSO_MATERIAL_ID, RECURSO_MATERIAL,
+                                                   ESTADO_ID, CREADO_POR, FECHA_CREACION)
+                       VALUES (?, ?, 1, ?, NOW());';
+
+        $query = $conectar->prepare($query);
+        $query->bindValue(1, $tipoRecursoMaterialID);
+        $query->bindValue(2, $recursoMaterial);
+        $query->bindValue(3, $creadoPor);
+        $query->execute();
+
+        return $resultado = $query->fetchAll();
+    }
+}
