610f8900ff2d7a11785c7b58f128a7bb9eb4bc05
andresgc04 <andresgc1997@outlook.com>
2024-06-15 19:23:41 -0400

    Creando SexosController
    Creando SexosController y agregando la funcionalidad para consumir como endpoint la funcion de obtener listados de opciones de sexos.

---
+++ controller/SexosController.php
@@ -0,0 +1,24 @@
+<?php
+require_once('../config/connection.php');
+require_once('../models/Sexos.php');
+require_once('../public/php/constants/sessions-constants.php');
+
+$creadoPor = $_SESSION[$USUARIO_ID];
+
+$sexos = new Sexos();
+
+switch ($_GET['op']) {
+    case 'obtener_listado_opciones_sexos':
+        $datos = $sexos->obtener_listado_opciones_sexos();
+
+        if (is_array($datos) == true and count($datos) > 0) {
+            $html .= '<option selected disabled>Por favor seleccione el sexo.</option>';
+
+            foreach ($datos as $row) {
+                $html .= '<option value="' . $row['SEXO_ID'] . '">' . $row['SEXO'] . '</option>';
+            }
+
+            echo $html;
+        }
+        break;
+}
