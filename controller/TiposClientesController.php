2475b3d068e3ec2c94abe31bb1c678430f2eabbb
andresgc04 <andresgc1997@outlook.com>
2024-06-15 19:12:16 -0400

    Creando TipoClienteController
    Creando TipoClienteController y agregando funcionalidad de obtener listado de opciones de tipos de clientes para consumirla como un endpoint.

---
+++ controller/TiposClientesController.php
@@ -0,0 +1,24 @@
+<?php
+require_once('../config/connection.php');
+require_once('../models/TiposClientes.php');
+require_once('../public/php/constants/sessions-constants.php');
+
+$creadoPor = $_SESSION[$USUARIO_ID];
+
+$tiposClientes = new TiposClientes();
+
+switch ($_GET['op']) {
+    case 'obtener_listado_opciones_tipos_clientes':
+        $datos = $tiposClientes->obtener_listado_opciones_tipos_clientes();
+
+        if (is_array($datos) == true and count($datos) > 0) {
+            $html .= '<option selected disabled>Por favor seleccione el tipo de cliente.</option>';
+
+            foreach ($datos as $row) {
+                $html .= '<option value="' . $row['TIPO_CLIENTE_ID'] . '">' . $row['TIPO_CLIENTE'] . '</option>';
+            }
+
+            echo $html;
+        }
+        break;
+}
