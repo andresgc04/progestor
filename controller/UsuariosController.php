ef14926ecb82cad48e2de5822fc6657bb7f97bb7
andresgc04 <andresgc1997@outlook.com>
2024-06-16 20:30:17 -0400

    Creando UsuariosController
    Creando endpoint de la funcionalidad para registrar nuevos usuarios clientes

---
+++ controller/UsuariosController.php
@@ -0,0 +1,37 @@
+<?php
+require_once('../config/connection.php');
+require_once('../models/Usuarios.php');
+require_once('../public/php/constants/sessions-constants.php');
+
+$creadoPor = $_SESSION[$USUARIO_ID];
+
+$usuarios = new Usuarios();
+
+//Codificar el password con hash:
+$hashed_password = password_hash($_POST['nuevoPassword'], PASSWORD_DEFAULT);
+
+switch ($_GET['op']) {
+    case 'registrar_usuarios_clientes':
+        $usuarios->registrar_usuarios_clientes(
+            $_POST['primerNombre'],
+            $_POST['segundoNombre'],
+            $_POST['primerApellido'],
+            $_POST['segundoApellido'],
+            $_POST['sexoID'],
+            $_POST['cedula'],
+            $_POST['fechaNacimiento'],
+            $_POST['nacionalidadID'],
+            $_POST['paisID'],
+            $_POST['provinciaID'],
+            $_POST['ciudadID'],
+            $_POST['direccion'],
+            $_POST['telefonoResidencial'],
+            $_POST['telefonoCelular'],
+            $_POST['correoElectronico'],
+            $_POST['tipoClienteID'],
+            $_POST['nuevoNombreUsuario'],
+            $_POST[$hashed_password],
+            $_POST[$creadoPor]
+        );
+        break;
+}
