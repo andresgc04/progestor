a6ea8c6fad03889f43fc96bd5680580f5232f4f8
andresgc04 <andresgc1997@outlook.com>
2024-06-17 22:29:41 -0400

    Creando Controlador De Empleados (EmpleadosController)
    Creando EndPoint De Obtener El Listado De Registros Existentes De Empleados

---
+++ controller/EmpleadosController.php
@@ -0,0 +1,48 @@
+<?php
+require_once('../config/connection.php');
+require_once('../models/Empleados.php');
+require_once('../public/php/constants/sessions-constants.php');
+
+$creadoPor = $_SESSION[$USUARIO_ID];
+
+$empleados = new Empleados();
+
+switch ($_GET['op']) {
+    case 'listado_empleados':
+        $datos = $empleados->listado_empleados();
+        $data = array();
+
+        foreach ($datos as $row) {
+            $sub_array = array();
+            $sub_array[] = $row['empleado_id'];
+            $sub_array[] = $row['empleados'];
+            $sub_array[] = $row['puestos'];
+            $sub_array[] = $row['departamentos'];
+            $sub_array[] = $row['supervisores'];
+            $sub_array[] = $row['fecha_contratacion'];
+
+            if ($row['ESTADO'] === "ACTIVO") {
+                $sub_array[] = '<span class="badge badge-primary">ACTIVO</span>';
+            }
+
+            $sub_array[] = '<td class="text-right py-0 align-middle">
+                                <div class="btn-group btn-group-sm">
+                                    <button type="button" id="' . $row['empleado_id'] . '" onClick="verDetalleEmpleados(' . $row['empleado_id'] . ')" class="btn btn-info"><i class="fas fa-eye"></i></button>
+                                    <button type="button" id="' . $row['empleado_id'] . '" onClick="eliminarEmpleados(' . $row['empleado_id'] . ')"class="btn btn-danger"><i class="fas fa-trash"></i></button>
+                                </div>
+                            </td>
+                            ';
+
+            $data[] = $sub_array;
+        }
+
+        $resultados = array(
+            "sEcho" => 1,
+            "iTotalRecords" => count($data),
+            "iTotalDisplayRecords" => count($data),
+            "aaData" => $data
+        );
+
+        echo json_encode($resultados);
+        break;
+}
