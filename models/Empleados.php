6b999c614149e828b16c59e8f001e74811311d20
andresgc04 <andresgc1997@outlook.com>
2024-06-17 22:08:17 -0400

    Creando Modelo De Empleados
    Agregando funcionalidad para obtener el listado de registros existentes de empleados.

---
+++ models/Empleados.php
@@ -0,0 +1,33 @@
+<?php
+class Empleados extends Connection
+{
+    public function listado_empleados()
+    {
+        $conectar = parent::Connection();
+        parent::set_names();
+
+        $query = "SELECT UCASE(CONCAT(empleados.primer_nombre, ' ', empleados.segundo_nombre, ' ',
+                         empleados.primer_apellido, ' ', empleados.segundo_apellido)) AS EMPLEADOS,
+                         UCASE(puestos.puesto) puestos, UCASE(departamentos.departamento) departamentos,
+                         UCASE(CONCAT(supervisores.primer_nombre, ' ', supervisores.segundo_nombre, '', 
+                         supervisores.primer_apellido, ' ', supervisores.segundo_apellido)) AS supervisores,
+                         empleados.fecha_contratacion, UCASE(estados.estado) estados
+                    FROM EMPLEADOS empleados
+                         INNER JOIN PUESTOS puestos
+                      ON empleados.puesto_id = puestos.puesto_id
+                         INNER JOIN DEPARTAMENTOS departamentos
+                      ON empleados.departamento_id = DEPARTAMENTOS.departamento_id
+                         INNER JOIN EMPLEADOS supervisores 
+                      ON empleados.supervisor_id = supervisores.empleado_id
+                         INNER JOIN ESTADOS estados
+                      ON empleados.estado_id = estados.estado_id
+                   WHERE empleados.estado_id = 1
+                ORDER BY empleados.empleado_id desc, empleados.fecha_creacion desc
+                 ";
+
+        $query = $conectar->prepare($query);
+        $query->execute();
+
+        return $resultado = $query->fetchAll();
+    }
+}
