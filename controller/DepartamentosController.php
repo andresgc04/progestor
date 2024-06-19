af1ae7cd7c89d6ab4c88708a1607b5ded3843b68
andresgc04 <andresgc1997@outlook.com>
2024-06-18 22:51:28 -0400

    Creando controlador de Departamentos (DepartamentosController)
    Agregando funcionalidad para consumir el endpoint para obtener el listado de opciones de departamentos y utilizarlos en los selects

---
+++ controller/DepartamentosController.php
@@ -0,0 +1,24 @@
+<?php
+require_once('../config/connection.php');
+require_once('../models/Departamentos.php');
+require_once('../public/php/constants/sessions-constants.php');
+
+$creadoPor = $_SESSION[$USUARIO_ID];
+
+$departamentos = new Departamentos();
+
+switch ($_GET['op']) {
+    case 'obtener_listado_opciones_departamentos':
+        $datos = $departamentos->obtener_listado_opciones_departamentos();
+
+        if (is_array($datos) == true and count($datos) > 0) {
+            $html .= '<option selected disabled>Por favor seleccione el departamento.</option>';
+
+            foreach ($datos as $row) {
+                $html .= '<option value="' . $row['DEPARTAMENTO_ID'] . '">"' . $row['DEPARTAMENTO'] . '"</option>';
+            }
+
+            echo $html;
+        }
+        break;
+}
