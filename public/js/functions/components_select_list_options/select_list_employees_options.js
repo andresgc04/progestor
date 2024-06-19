d436b59b7a2cb4a222c3ea8a6ed966d2cf8de883
andresgc04 <andresgc1997@outlook.com>
2024-06-18 23:48:56 -0400

    Creando componente de funcionalidad para poder consumir y mostrar en los select las opciones de supervisores

---
+++ public/js/functions/components_select_list_options/select_list_employees_options.js
@@ -0,0 +1,45 @@
+const getSelectListEmployeesOptions = (urlController, inputFieldID) => {
+  $.post(urlController)
+    .done(function (data, status) {
+      $(inputFieldID).html(data);
+    })
+    .fail(function (data, status) {
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "Ocurrio un error inesperado",
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true,
+      }).then(
+        (willClose = () => {
+          window.location.reload();
+        })
+      );
+    });
+};
+
+const getSelectListSupervisorsOptionsByDepartamentoID = (
+  urlController,
+  departamentoID,
+  inputFieldID
+) => {
+  $.post(urlController, { departamentoID: departamentoID })
+    .done(function (data, status) {
+      $(inputFieldID).html(data);
+    })
+    .fail(function (data, status) {
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "Ocurrio un error inesperado",
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true,
+      }).then(
+        (willClose = () => {
+          window.location.reload();
+        })
+      );
+    });
+};
