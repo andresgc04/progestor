fd14a20e6fd44e432ec00aedf672c50ac1d9ac25
andresgc04 <andresgc1997@outlook.com>
2024-06-19 23:02:32 -0400

    Creando Modelo De Solicitudes De Proyectos
    Agregando funcionalidad para obtener el listado de proyectos existentes

---
+++ models/SolicitudesProyectos.php
@@ -0,0 +1,30 @@
+<?php
+class SolicitudesProyectos extends Connection
+{
+    public function listado_solicitudes_proyectos()
+    {
+        $conectar = parent::Connection();
+        parent::set_names();
+
+        $query = "SELECT UCASE(solicitudesProyectos.descripcion_proyecto) AS descripcion_proyecto,
+                         UCASE(solicitudesProyectos.objetivo_proyecto) AS objetivo_proyecto,
+                         UCASE(solicitudesProyectos.creado_por) AS solicitado_por,
+                         DATE_FORMAT(solicitudesProyectos.fecha_creacion, '%d/%m/%Y') AS fecha_solicitud,
+                         UCASE(estados.estado) AS estado
+                    FROM SOLICITUDES_PROYECTOS solicitudesProyectos
+              INNER JOIN USUARIOS usuarios
+                      ON solicitudesProyectos.creado_por = usuarios.usuario_id
+                         INNER JOIN CLIENTES clientes
+                      ON usuarios.cliente_id = clientes.cliente_id
+                         INNER JOIN ESTADOS estados
+                      ON solicitudesProyectos.estado_id = estados.estado_id
+                   WHERE solicitudesProyectos.creado_por = ''
+                ORDER BY solicitudesProyectos.solicitud_proyecto_id DESC, 
+                         solicitudesProyectos.fecha_creacion DESC;";
+
+        $query = $conectar->prepare($query);
+        $query->execute();
+
+        return $resultado = $query->fetchAll();
+    }
+}
