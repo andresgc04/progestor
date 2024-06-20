fc38b2fe8c0a1ed67c09036361e34b87b5f56ca4
andresgc04 <andresgc1997@outlook.com>
2024-06-20 15:33:08 -0400

    Agregar funcionalidad de mostrar el listado de usuarios asignados a los empleados en la bandeja de entrada a los usuarios asignados a los empleados

--- view/home-assign-user-employee/home-assign-user-employee.js
+++ view/home-assign-user-employee/home-assign-user-employee.js
@@ -7,3 +7,61 @@ setContentHeaderTitle("Listado De Usuarios Asignados A Empleados");
 setBreadCrumbContentHeaderTitle("../dashboard/", "Dashboard");
 
 setBreadCrumbContentHeaderSubTitle("Listado De Usuarios Asignados A Empleados");
+
+const obtenerListadoUsuariosAsignadosEmpleadosDataTable = () => {
+  $("#listadoUsuariosAsignadosEmpleadosDataTable")
+    .dataTable({
+      aProcessing: true,
+      aServerSide: true,
+      dom: "Bfrtip",
+      searching: true,
+      lengthChange: false,
+      colReorder: true,
+      buttons: ["copyHtml5", "excelHtml5", "csvHtml5", "pdfHtml5"],
+      ajax: {
+        url: "../../controller/UsuariosController.php?op=listado_usuarios_asignados_empleados",
+        type: "post",
+        dataType: "json",
+        error: function (e) {
+          console.log(e.responseText);
+        },
+      },
+      ordering: true,
+      bDestroy: true,
+      responsive: true,
+      bInfo: true,
+      iDisplayLength: 10,
+      autoWidth: false,
+      language: {
+        sProcessing: "Procesando...",
+        sLengthMenu: "Mostrar _MENU_ registros",
+        sZeroRecords: "No se encontraron resultados",
+        sEmptyTable: "Ningun dato disponible en esta tabla",
+        sInfo: "Mostrando un total de _TOTAL_ registros",
+        sInfoEmpty: "Mostrando un total de 0 registros",
+        sInfoFiltered: "(filtrado de un total de _MAX_ registros)",
+        sInfoPostFix: "",
+        sSearch: "Buscar:",
+        sUrl: "",
+        sInfoThousands: ",",
+        sLoadingRecords: "Cargando....",
+        oPaginate: {
+          sFirst: "Primero",
+          sLast: "Ultimo",
+          sNext: "Siguiente",
+          sPrevious: "Anterior",
+        },
+        oAria: {
+          sSortAscending:
+            ": Activar para ordenar la columna de manera ascendente",
+          sSortDescending:
+            ": Activar para ordenar la columna de manera descendente",
+        },
+      },
+    })
+    .DataTable();
+};
+
+(function () {
+  obtenerListadoUsuariosAsignadosEmpleadosDataTable();
+})();
