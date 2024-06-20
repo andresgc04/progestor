028d43f4683a38ca802aa777169e4f1ca6f03c67
andresgc04 <andresgc1997@outlook.com>
2024-06-20 11:26:14 -0400

    Agregando Funcionalidad De Registrar Solicitudes De Proyectos
    Agregando funcionalidad para registrar solicitudes de proyectos en el modelo de solicitudes de proyectos

--- models/SolicitudesProyectos.php
+++ models/SolicitudesProyectos.php
@@ -1,12 +1,63 @@
 <?php
 class SolicitudesProyectos extends Connection
 {
-    public function listado_solicitudes_proyectos_por_usuarioID($usuarioID)
-    {
-        $conectar = parent::Connection();
-        parent::set_names();
+   public function registrar_solicitudes_proyectos(
+      $descripcionProyecto,
+      $objetivoProyecto,
+      $presupuestoProyecto,
 
-        $query = "SELECT solicitudesProyectos.solicitud_proyecto_id,
+      $descripcionRequerimiento,
+
+      $creadoPor
+   ) {
+      $conectar = parent::Connection();
+      parent::set_names();
+
+      $queryInsertarSolicitudesProyectos = 'INSERT INTO SOLICITUDES_PROYECTOS (descripcion_proyecto,
+                                                                               objetivo_proyecto,
+                                                                               presupuesto_proyecto,
+                                                                               estado_id,
+                                                                               creado_por,
+                                                                               fecha_creacion
+                                                                              )
+                                                                        VALUES(?, ?, ?, 1, ?, NOW());';
+
+      $queryInsertarSolicitudesProyectos = $conectar->prepare($queryInsertarSolicitudesProyectos);
+      $queryInsertarSolicitudesProyectos->bindValue(1, $descripcionProyecto);
+      $queryInsertarSolicitudesProyectos->bindValue(2, $objetivoProyecto);
+      $queryInsertarSolicitudesProyectos->bindValue(3, $presupuestoProyecto);
+      $queryInsertarSolicitudesProyectos->bindValue(4, $creadoPor);
+      $queryInsertarSolicitudesProyectos->execute();
+
+      $solicitudProyectoID = $conectar->lastInsertId();
+
+      $countDescripcionRequerimiento = count($descripcionRequerimiento);
+
+      for ($requerimientoSolicitudProyectoIndex = 0; $requerimientoSolicitudProyectoIndex <= $countDescripcionRequerimiento; $requerimientoSolicitudProyectoIndex++) {
+         $queryInsertarRequerimientosSolicitudesProyectos = 'INSERT INTO REQUERIMIENTOS_SOLICITUDES_PROYECTOS (solicitud_proyecto_id,
+                                                                                                               descripcion_requerimiento,
+                                                                                                               estado_id,
+                                                                                                               creado_por,
+                                                                                                               fecha_creacion
+                                                                                                               )
+                                                                                                         VALUES(?, ?, 1, ?, NOW());';
+
+         $queryInsertarRequerimientosSolicitudesProyectos = $conectar->prepare($queryInsertarRequerimientosSolicitudesProyectos);
+         $queryInsertarRequerimientosSolicitudesProyectos->bindValue(1, $solicitudProyectoID);
+         $queryInsertarRequerimientosSolicitudesProyectos->bindValue(2, $descripcionRequerimiento[$requerimientoSolicitudProyectoIndex]);
+         $queryInsertarRequerimientosSolicitudesProyectos->bindValue(3, $creadoPor);
+         $queryInsertarRequerimientosSolicitudesProyectos->execute();
+      }
+
+      return $resultado = $queryInsertarRequerimientosSolicitudesProyectos->fetchAll();
+   }
+
+   public function listado_solicitudes_proyectos_por_usuarioID($usuarioID)
+   {
+      $conectar = parent::Connection();
+      parent::set_names();
+
+      $query = "SELECT solicitudesProyectos.solicitud_proyecto_id,
                          UCASE(solicitudesProyectos.descripcion_proyecto) AS descripcion_proyecto,
                          UCASE(solicitudesProyectos.objetivo_proyecto) AS objetivo_proyecto,
                          UCASE(solicitudesProyectos.creado_por) AS solicitado_por,
@@ -23,10 +74,10 @@ class SolicitudesProyectos extends Connection
                 ORDER BY solicitudesProyectos.solicitud_proyecto_id DESC, 
                          solicitudesProyectos.fecha_creacion DESC;";
 
-        $query = $conectar->prepare($query);
-        $query->bindValue(1, $usuarioID);
-        $query->execute();
+      $query = $conectar->prepare($query);
+      $query->bindValue(1, $usuarioID);
+      $query->execute();
 
-        return $resultado = $query->fetchAll();
-    }
+      return $resultado = $query->fetchAll();
+   }
 }
