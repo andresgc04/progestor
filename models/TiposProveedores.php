a963b73455f62ec9b02e6235776880e05d009877
andresgc04 <andresgc1997@outlook.com>
2024-06-23 16:53:27 -0400

    Modificando Modelo De TiposProveedores
    Agregando funcionalidad para registrar los nuevos proveedores

--- models/TiposProveedores.php
+++ models/TiposProveedores.php
@@ -1,6 +1,22 @@
 <?php
 class TiposProveedores extends Connection
 {
+    public function registrar_tipos_proveedores($tipoProveedor, $creadoPor)
+    {
+        $conectar = parent::Connection();
+        parent::set_names();
+
+        $query = 'INSERT INTO TIPOS_PROVEEDORES (tipo_proveedor, estado_id, creado_por, fecha_creacion)
+                                          VALUES(?, 1, ?, NOW());';
+
+        $query = $conectar->prepare($query);
+        $query->bindValue(1, $tipoProveedor);
+        $query->bindValue(2, $creadoPor);
+        $query->execute();
+
+        return $resultado = $query->fetchAll();
+    }
+
     public function listado_tipos_proveedores()
     {
         $conectar = parent::Connection();
