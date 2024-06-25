0bef909e80080d33d3975a62eb91d92684a737c8
andresgc04 <andresgc1997@outlook.com>
2024-06-25 14:21:00 -0400

    Modificando Modelo De Proveedores
    Agregando funcionalidad para registrar nuevos proveedores

--- models/Proveedores.php
+++ models/Proveedores.php
@@ -1,12 +1,57 @@
 <?php
 class Proveedores extends Connection
 {
-    public function listado_proveedores()
-    {
-        $conectar = parent::Connection();
-        parent::set_names();
+   public function registrar_proveedores(
+      $nombreProveedor,
+      $paisID,
+      $provinciaID,
+      $ciudadID,
+      $direccion,
+      $telefono,
+      $correoElectronico,
+      $condicionPagoID,
+      $tipoProveedorID,
+      $representanteVentas,
+      $contactoRepresentanteVentas,
+      $creadoPor
+   ) {
+      $conectar = parent::Connection();
+      parent::set_names();
 
-        $query = 'SELECT proveedores.proveedor_id,
+      $query = 'INSERT INTO PROVEEDORES (nombre_proveedor, pais_id, provincia_id, ciudad_id, direccion,
+                                         telefono, correo_electronico, condicion_pago_id, tipo_proveedor_id,
+                                         representante_ventas, contacto_representante_ventas, estado_id, creado_por, 
+                                         fecha_creacion
+                                        )
+                                  VALUES(?, ?, ?, ?, ?,
+                                         ?, ?, ?, ?, ?,
+                                         ?, 1, ?, NOW()
+                                        );';
+
+      $query = $conectar->prepare($query);
+      $query->bindValue(1, $nombreProveedor);
+      $query->bindValue(2, $paisID);
+      $query->bindValue(3, $provinciaID);
+      $query->bindValue(4, $ciudadID);
+      $query->bindValue(5, $direccion);
+      $query->bindValue(6, $telefono);
+      $query->bindValue(7, $correoElectronico);
+      $query->bindValue(8, $condicionPagoID);
+      $query->bindValue(9, $tipoProveedorID);
+      $query->bindValue(10, $representanteVentas);
+      $query->bindValue(11, $contactoRepresentanteVentas);
+      $query->bindValue(12, $creadoPor);
+      $query->execute();
+
+      return $resultado = $query->fetchAll();
+   }
+
+   public function listado_proveedores()
+   {
+      $conectar = parent::Connection();
+      parent::set_names();
+
+      $query = 'SELECT proveedores.proveedor_id,
                          UCASE(proveedores.nombre_proveedor) proveedor,
                          UCASE(tiposProveedores.tipo_proveedor) tipo_proveedor,
                          UCASE(estados.estado) estado
@@ -18,9 +63,9 @@ class Proveedores extends Connection
                    WHERE proveedores.estado_id = 1
                 ORDER BY proveedores.proveedor_id DESC, proveedores.fecha_creacion DESC;';
 
-        $query = $conectar->prepare($query);
-        $query->execute();
+      $query = $conectar->prepare($query);
+      $query->execute();
 
-        return $resultado = $query->fetchAll();
-    }
+      return $resultado = $query->fetchAll();
+   }
 }
