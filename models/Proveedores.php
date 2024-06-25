3967350153998c3ddaa4c93f012e16b718e57661
andresgc04 <andresgc1997@outlook.com>
2024-06-24 22:03:58 -0400

    Creando Modelo De Proveedores
    Agregando funcionalidad para obtener listado de registros existentes de los proveedores.

---
+++ models/Proveedores.php
@@ -0,0 +1,25 @@
+<?php
+class Proveedores extends Connection
+{
+    public function listado_proveedores()
+    {
+        $conectar = parent::Connection();
+        parent::set_names();
+
+        $query = 'SELECT proveedores.proveedor_id,
+                         UCASE(tiposProveedores.tipo_proveedor) tipo_proveedor,
+                         UCASE(estados.estado) estado
+                    FROM PROVEEDORES proveedores
+                         INNER JOIN TIPOS_PROVEEDORES tiposProveedores
+                      ON proveedores.tipo_proveedor_id = tiposProveedores.tipo_proveedor_id
+                         INNER JOIN ESTADOS estados
+                      ON proveedores.estado_id = estados.estado_id
+                   WHERE proveedores.estado_id = 1
+                ORDER BY proveedores.proveedor_id DESC, proveedores.fecha_creacion DESC;';
+
+        $query = $conectar->prepare($query);
+        $query->execute();
+
+        return $resultado = $query->fetchAll();
+    }
+}
