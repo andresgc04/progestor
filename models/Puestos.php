3e72b62f72ac305e6934e873bd52818804f3f916
andresgc04 <andresgc1997@outlook.com>
2024-06-29 13:49:24 -0400

    Modificando Modelo De Puestos
    Agregando Funcionalidad Para Obtener El Listado De Puestos Existentes

--- models/Puestos.php
+++ models/Puestos.php
@@ -1,6 +1,23 @@
 <?php
 class Puestos extends Connection
 {
+    public function listado_puestos()
+    {
+        $conectar = parent::Connection();
+        parent::set_names();
+
+        $query = 'SELECT PUESTO_ID,
+                         PUESTO
+                    FROM PUESTOS
+                   WHERE ESTADO_ID = 1
+                ORDER BY PUESTO_ID DESC, FECHA_CREACION DESC;';
+
+        $query = $conectar->prepare($query);
+        $query->execute();
+
+        return $resultado = $query->fetchAll();
+    }
+
     public function obtener_listado_opciones_puestos()
     {
         $conectar = parent::Connection();
