89beaefbc8cbe80612b6ec2d574dd897dd06df4a
andresgc04 <andresgc1997@outlook.com>
2024-06-29 11:47:02 -0400

    Modificando Controlador De Roles (RolesController)
    Agregando endpoint para consumir los datos del listado de roles existentes.

--- controller/RolesController.php
+++ controller/RolesController.php
@@ -8,6 +8,38 @@ $creadoPor = $_SESSION[$USUARIO_ID];
 $roles = new Roles();
 
 switch ($_GET['op']) {
+    case "listado_roles":
+        $datos = $roles->listado_roles();
+        $data = array();
+
+        foreach ($datos as $row) {
+            $sub_array = array();
+            $sub_array[] = $row['ROL_ID'];
+            $sub_array[] = $row['ROL'];
+
+            if ($row["ESTADO"] === "ACTIVO") {
+                $sub_array[] = '<span class="badge badge-primary">ACTIVO</span>';
+            }
+
+            $sub_array[] = '<td class="text-right py-0 align-middle">
+                                <div class="btn-group btn-group-sm">
+                                    <button type="button" id="' . $row['ROL_ID'] . '" onclick="verDetalleRol(' . $row['ROL_ID'] . ')" class="btn btn-info"><i class="fas fa-eye"></i></button>
+                                    <button type="button" id="' . $row['ROL_ID'] . '" onclick="eliminarRol(' . $row['ROL_ID'] . ')" class="btn btn-danger"><i class="fas fa-trash"></i></button>
+                                </div>
+                            </td>';
+
+            $data[] = $sub_array;
+        }
+
+        $resultados = array(
+            "sEcho" => 1,
+            "iTotalRecords" => count($data),
+            "iTotalDisplayRecords" => count($data),
+            "aaData" => $data
+        );
+
+        echo json_encode($resultados);
+        break;
     case 'obtener_listado_opciones_roles':
         $datos = $roles->obtener_listado_opciones_roles();
 
