d284e5da9d43b5e515b824a7becb428b80c84eaf
andresgc04 <andresgc1997@outlook.com>
2024-06-29 12:47:25 -0400

    Modificando Modelo De Roles
    Agregando Funcionalidad Para Registrar Nuevos Roles

--- models/Roles.php
+++ models/Roles.php
@@ -1,6 +1,22 @@
 <?php
 class Roles extends Connection
 {
+    public function registrar_roles($rol, $creadoPor)
+    {
+        $conectar = parent::Connection();
+        parent::set_names();
+
+        $query = 'INSERT INTO ROLES (ROL, ESTADO_ID, CREADO_POR, FECHA_CREACION)
+      		                  VALUES(?, 1, ?, NOW());';
+
+        $query = $conectar->prepare($query);
+        $query->bindValue(1, $rol);
+        $query->bindValue(2, $creadoPor);
+        $query->execute();
+
+        return $resultado = $query->fetchAll();
+    }
+
     public function listado_roles()
     {
         $conectar = parent::Connection();
