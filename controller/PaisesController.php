b61ba2b24a02c246f456475ae50df13327a7fd35
andresgc04 <andresgc1997@outlook.com>
2024-06-30 14:01:27 -0400

    Modificando Controlador De Paises (PaisesController)
    Agregando endpoint para consumir y obtener el detalle del pais por paisID

--- controller/PaisesController.php
+++ controller/PaisesController.php
@@ -56,4 +56,38 @@ switch ($_GET["op"]) {
             echo $html;
         }
         break;
+    case 'obtener_detalle_pais_por_paisID':
+        $data = $paises->obtener_detalle_pais_por_paisID($_POST['paisID']);
+
+        if (is_array($data) == true and count($data) > 0) {
+            // Normalizar la estructura de los datos si es necesario
+            $normalizedData = array();
+
+            foreach ($data as $item) {
+                // Si los datos son un array asociativo con claves numéricas y asociativas,
+                // seleccionar las claves que deseas mantener o normalizar la estructura según sea necesario.
+                $normalizedItem = [
+                    'paisID' => $item['PAIS_ID'],
+                    'pais' => $item['PAIS']
+                ];
+
+                // Agregar el elemento normalizado al array resultante
+                $normalizedData = $normalizedItem;
+            }
+
+            // Crear un array asociativo con la clave "data"
+            $response = array('data' => $normalizedData);
+
+            // Convertir el array de objetos a formato JSON:
+            $json = json_encode($response, JSON_UNESCAPED_UNICODE);
+
+            // Configurar la cabecera para indicar que la respuesta es JSON
+            header('Content-Type: application/json');
+
+            // Retornar o imprimir el JSON
+            echo $json;
+        } else {
+            echo json_encode(['data' => []]);
+        }
+        break;
 }
