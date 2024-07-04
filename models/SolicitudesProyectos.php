965211c5de40e83e6a7bf46c2fbede8281acdda0
andresgc04 <andresgc1997@outlook.com>
2024-07-04 00:14:23 -0400

    Modificando Solicitudes De Proyectos

--- models/SolicitudesProyectos.php
+++ models/SolicitudesProyectos.php
@@ -75,21 +75,21 @@ class SolicitudesProyectos extends Connection
       parent::set_names();
 
       $query = "SELECT solicitudesProyectos.solicitud_proyecto_id,
-                         UCASE(solicitudesProyectos.descripcion_proyecto) AS descripcion_proyecto,
-                         UCASE(solicitudesProyectos.objetivo_proyecto) AS objetivo_proyecto,
-                         UCASE(solicitudesProyectos.creado_por) AS solicitado_por,
-                         DATE_FORMAT(solicitudesProyectos.fecha_creacion, '%d/%m/%Y') AS fecha_solicitud,
-                         UCASE(estados.estado) AS estado
-                    FROM SOLICITUDES_PROYECTOS solicitudesProyectos
-              INNER JOIN USUARIOS usuarios
-                      ON solicitudesProyectos.creado_por = usuarios.usuario_id
-                         INNER JOIN CLIENTES clientes
-                      ON usuarios.cliente_id = clientes.cliente_id
-                         INNER JOIN ESTADOS estados
-                      ON solicitudesProyectos.estado_id = estados.estado_id
-                   WHERE solicitudesProyectos.creado_por = ?
-                ORDER BY solicitudesProyectos.solicitud_proyecto_id DESC, 
-                         solicitudesProyectos.fecha_creacion DESC;";
+                       UCASE(solicitudesProyectos.descripcion_proyecto) AS descripcion_proyecto,
+                       UCASE(solicitudesProyectos.objetivo_proyecto) AS objetivo_proyecto,
+                       UCASE(clientes.nombre_cliente) AS solicitado_por,
+                       DATE_FORMAT(solicitudesProyectos.fecha_creacion, '%d/%m/%Y') AS fecha_solicitud,
+                       UCASE(estados.estado) AS estado
+                  FROM SOLICITUDES_PROYECTOS solicitudesProyectos
+            INNER JOIN USUARIOS usuarios
+                    ON solicitudesProyectos.creado_por = usuarios.usuario_id
+            INNER JOIN CLIENTES clientes
+                    ON usuarios.cliente_id = clientes.cliente_id
+            INNER JOIN ESTADOS estados
+                    ON solicitudesProyectos.estado_id = estados.estado_id
+                 WHERE solicitudesProyectos.creado_por = ?
+              ORDER BY solicitudesProyectos.solicitud_proyecto_id DESC, 
+                       solicitudesProyectos.fecha_creacion DESC;";
 
       $query = $conectar->prepare($query);
       $query->bindValue(1, $usuarioID);
