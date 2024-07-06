88001e8ceacd3e71d9c1dcea8072d6a748c7160a
andresgc04 <andresgc1997@outlook.com>
2024-07-06 10:48:52 -0400

    Modificando Controlador De Solicitudes De Proyectos (SolicitudesProyectosController.php
    Agregando funcionalidad para obtener el encabezado de las solicitudes de los proyectos

--- controller/SolicitudesProyectosController.php
+++ controller/SolicitudesProyectosController.php
@@ -52,4 +52,41 @@ switch ($_GET['op']) {
 
         echo json_encode($resultados);
         break;
+    case 'obtener_encabezado_solicitudes_proyectos_por_solicitud_proyecto_ID':
+        $data = $solicitudesProyectos->obtener_encabezado_solicitudes_proyectos_por_solicitud_proyecto_ID($_POST['solicitudProyectoID']);
+
+        if (is_array($data) == true and count($data) > 0) {
+            // Normalizar la estructura de los datos si es necesario
+            $normalizedData = array();
+
+            foreach ($data as $item) {
+                // Si los datos son un array asociativo con claves numéricas y asociativas,
+                // seleccionar las claves que deseas mantener o normalizar la estructura según sea necesario.
+                $normalizedItem = [
+                    'puestoID' => $item['SOLICITUD_PROYECTO_ID'],
+                    'puesto' => $item['DESCRIPCION_PROYECTO'],
+                    'objetivoProyecto' => $item['OBJETIVO_PROYECTO'],
+                    'presupuestoProyecto' => $item['PRESUPUESTO_PROYECTO'],
+                    'estado' => $item['ESTADO']
+                ];
+
+                // Agregar el elemento normalizado al array resultante
+                $normalizedData = $normalizedItem;
+            }
+
+            // Crear un array asociativo con la clave "data"
+            $response = array('data' => $normalizedData);
+
+            // Convertir el array de objetos a formato JSON:
+            $json = json_encode($response, JSON_UNESCAPED_UNICODE);
+
+            // Configurar la cabecera para indicar que la respuesta es JSON
+            header('Content-Type: application/json');
+
+            // Retornar o imprimir el JSON
+            echo $json;
+        } else {
+            echo json_encode(['data' => []]);
+        }
+        break;
 }
