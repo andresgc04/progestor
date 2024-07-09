70871395e5f2d4df0f98d9af69577ea737fdaebf
andresgc04 <andresgc1997@outlook.com>
2024-07-08 22:07:49 -0400

    Modificando Controlador De Solicitudes De Proyectos (SolicitudesProyectosController)
    Agregando EndPoint para consumir funcionalidad para obtener el requerimiento de una solicitud de proyecto por solicitudProyectoID y requerimientoSolicitudProyectoID

--- controller/SolicitudesProyectosController.php
+++ controller/SolicitudesProyectosController.php
@@ -180,4 +180,42 @@ switch ($_GET['op']) {
             $creadoPor
         );
         break;
+    case 'obtener_requerimiento_solicitud_proyecto_por_solicitud_proyecto_ID_requerimiento_solicitud_proyecto_ID':
+        $data = $solicitudesProyectos->obtener_requerimiento_solicitud_proyecto_por_solicitud_proyecto_ID_requerimiento_solicitud_proyecto_ID(
+            $_POST['solicitudProyectoID'],
+            $_POST['requerimientoSolicitudProyectoID']
+        );
+
+        if (is_array($data) == true and count($data) > 0) {
+            // Normalizar la estructura de los datos si es necesario
+            $normalizedData = array();
+
+            foreach ($data as $item) {
+                // Si los datos son un array asociativo con claves numéricas y asociativas,
+                // seleccionar las claves que deseas mantener o normalizar la estructura según sea necesario.
+                $normalizedItem = [
+                    'solicitudProyectoID' => $item['SOLICITUD_PROYECTO_ID'],
+                    'requerimientoSolicitudProyectoID' => $item['REQUERIMIENTO_SOLICITUD_PROYECTO_ID'],
+                    'descripcionRequerimiento' => $item['DESCRIPCION_REQUERIMIENTO'],
+                ];
+
+                // Agregar el elemento normalizado al array resultante
+                $normalizedData = $normalizedItem;
+            }
+
+            // Crear un array asociativo con la clave "data"
+            $response = array('data' => $normalizedData);
+
+            // Convertir el array de objetos a formato JSON:
+            $json = json_encode($response, JSON_UNESCAPED_UNICODE);
+
+            // Configurar la cabecera para indicar que la respuesta es JSON
+            header('Content-Type: application/json');
+
+            // Retornar o imprimir el JSON
+            echo $json;
+        } else {
+            echo json_encode(['data' => []]);
+        }
+        break;
 }
