f0969ac0cdacac0b90ea1c6202598abafe4944b1
andresgc04 <andresgc1997@outlook.com>
2024-07-11 00:44:45 -0400

    Creando Modelo De Categorias De TIpos De Proyectos
    Agregando funcionalidad para obtener el listado de opciones de categorias De Tipos De Proyectos

---
+++ models/CategoriasTiposProyectosObrasCiviles.php
@@ -0,0 +1,22 @@
+<?php
+class CategoriasTiposProyectosObrasCiviles extends Connection
+{
+    public function obtener_listado_opciones_categorias_tipos_proyectos_obras_civiles_por_tipo_proyecto_obra_civil_ID($tipoProyectoObraCivilID)
+    {
+        $conectar = parent::Connection();
+        parent::set_names();
+
+        $query = 'SELECT CATEGORIA_TIPO_PROYECTO_OBRA_CIVIL_ID,
+                         CATEGORIA_TIPO_PROYECTO_OBRA_CIVIL
+                    FROM CATEGORIAS_TIPOS_PROYECTOS_OBRAS_CIVILES
+                   WHERE TIPO_PROYECTO_OBRA_CIVIL_ID = ? AND ESTADO_ID = 1;';
+
+        $query = $conectar->prepare($query);
+        $query->bindValue(1, $tipoProyectoObraCivilID);
+        $query->execute();
+
+        $resultado = $query->fetchAll();
+
+        return $resultado;
+    }
+}
