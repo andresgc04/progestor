890a70146b3bf07c992d916a9accbb60920c7e11
andresgc04 <andresgc1997@outlook.com>
2024-07-11 00:57:46 -0400

    Agregando Controlador De TiposProyectosObrasCiviles (TiposProyectosObrasCivilesController)
    Agregando endpoint para consumir la funcionalidad para obtener el listado de opciones de tipos de proyectos obras civiles

---
+++ controller/TiposProyectosObrasCivilesController.php
@@ -0,0 +1,24 @@
+<?php
+require_once('../config/connection.php');
+require_once('../models/TiposProyectosObrasCiviles.php');
+require_once('../public/php/constants/sessions-constants.php');
+
+$creadoPor = $_SESSION[$USUARIO_ID];
+
+$tiposProyectosObrasCiviles = new TiposProyectosObrasCiviles();
+
+switch ($_GET['op']) {
+    case "obtener_listado_opciones_tipos_proyectos_obras_civiles":
+        $datos = $tiposProyectosObrasCiviles->obtener_listado_opciones_tipos_proyectos_obras_civiles();
+
+        if (is_array($datos) == true and count($datos) > 0) {
+            $html .= "<option selected disabled>Por favor seleccione el tipo del proyecto.</option>";
+
+            foreach ($datos as $row) {
+                $html .= "<option value='" . $row['TIPO_PROYECTO_OBRA_CIVIL_ID'] . "'>" . $row['TIPO_PROYECTO_OBRA_CIVIL'] . "</option>";
+            }
+
+            echo $html;
+        }
+        break;
+}
