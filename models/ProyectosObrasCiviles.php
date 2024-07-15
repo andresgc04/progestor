eaaa991914f989955d52ad31e5ed35ee04b25293
andresgc04 <andresgc1997@outlook.com>
2024-07-14 23:58:14 -0400

    Modificando Modelo De Proyectos Obras Civiles
    Agregando funcionalidad para obtener datos de los proyectos de obras civiles

--- models/ProyectosObrasCiviles.php
+++ models/ProyectosObrasCiviles.php
@@ -72,4 +72,30 @@ class ProyectosObrasCiviles extends Connection
 
         return $resultado;
     }
+
+    public function obtener_datos_proyectos_obras_civiles_por_proyecto_obra_civil_ID_solicitud_proyecto_ID($proyectoObraCivilID, $solicitudProyectoID)
+    {
+        $conectar = parent::Connection();
+        parent::set_names();
+
+        $query = 'SELECT proyectosObrasCiviles.PROYECTO_OBRA_CIVIL_ID, proyectosObrasCiviles.SOLICITUD_PROYECTO_ID,
+                         UCASE(proyectosObrasCiviles.NOMBRE_PROYECTO) NOMBRE_PROYECTO, UCASE(proyectosObrasCiviles.DESCRIPCION_PROYECTO) DESCRIPCION_PROYECTO,
+                         proyectosObrasCiviles.TIPO_PROYECTO_OBRA_CIVIL_ID, proyectosObrasCiviles.CATEGORIA_TIPO_PROYECTO_OBRA_CIVIL_ID,
+                         proyectosObrasCiviles.RESPONSABLE_ID, proyectosObrasCiviles.FECHA_INICIO_PROYECTO,
+                         proyectosObrasCiviles.FECHA_FINALIZACION_PROYECTO, UCASE(estados.ESTADO) ESTADO
+                    FROM PROYECTOS_OBRAS_CIVILES proyectosObrasCiviles
+                         INNER JOIN ESTADOS estados
+                      ON proyectosObrasCiviles.ESTADO_ID = estados.ESTADO_ID
+                   WHERE proyectosObrasCiviles.PROYECTO_OBRA_CIVIL_ID = ? 
+                     AND proyectosObrasCiviles.SOLICITUD_PROYECTO_ID = ?;';
+
+        $query = $conectar->prepare($query);
+        $query->bindValue(1, $proyectoObraCivilID);
+        $query->bindValue(2, $solicitudProyectoID);
+        $query->execute();
+
+        $resultado = $query->fetchAll();
+
+        return $resultado;
+    }
 }
