f6d62a4e6797776d6551710f2125906d440857fe
andresgc04 <andresgc1997@outlook.com>
2024-07-16 22:52:24 -0400

    Modificando Modelo De CategoriasTiposProyectosObrasCiviles
    Agregando funcionalidad para obtener opciones de categorias tipos proyectos obras civiles por categoriaTipoProyectoObraCivilID y tipoProyectoObraCivilID como por igual diferente a CategoriaTipoProyectoObraCivilID y igual a tipoProyectoObraCivilID seleccionados o indicados

--- models/CategoriasTiposProyectosObrasCiviles.php
+++ models/CategoriasTiposProyectosObrasCiviles.php
@@ -19,4 +19,50 @@ class CategoriasTiposProyectosObrasCiviles extends Connection
 
         return $resultado;
     }
+
+    public function obtener_listado_opciones_categorias_tipos_proyectos_obras_civiles_por_categoria_tipo_proyecto_obra_civil_ID_tipo_proyecto_obra_civil_ID(
+        $categoriaTipoProyectoObraCivilID,
+        $tipoProyectoObraCivilID
+    ) {
+        $conectar = parent::Connection();
+        parent::set_names();
+
+        $query = "SELECT CATEGORIA_TIPO_PROYECTO_OBRA_CIVIL_ID,
+                         UCASE(CATEGORIA_TIPO_PROYECTO_OBRA_CIVIL) CATEGORIA_TIPO_PROYECTO_OBRA_CIVIL
+                    FROM CATEGORIAS_TIPOS_PROYECTOS_OBRAS_CIVILES
+                   WHERE CATEGORIA_TIPO_PROYECTO_OBRA_CIVIL_ID = ? 
+                     AND TIPO_PROYECTO_OBRA_CIVIL_ID = ?;";
+
+        $query = $conectar->prepare($query);
+        $query->bindValue(1, $categoriaTipoProyectoObraCivilID);
+        $query->bindValue(2, $tipoProyectoObraCivilID);
+        $query->execute();
+
+        $resultado = $query->fetchAll();
+
+        return $resultado;
+    }
+
+    public function obtener_listado_opciones_categorias_tipos_proyectos_obras_civiles_diferente_categoria_tipo_proyecto_obra_civil_ID_tipo_proyecto_obra_civil_ID(
+        $categoriaTipoProyectoObraCivilID,
+        $tipoProyectoObraCivilID
+    ) {
+        $conectar = parent::Connection();
+        parent::set_names();
+
+        $query = "SELECT CATEGORIA_TIPO_PROYECTO_OBRA_CIVIL_ID,
+                         UCASE(CATEGORIA_TIPO_PROYECTO_OBRA_CIVIL) CATEGORIA_TIPO_PROYECTO_OBRA_CIVIL
+                    FROM CATEGORIAS_TIPOS_PROYECTOS_OBRAS_CIVILES
+                   WHERE CATEGORIA_TIPO_PROYECTO_OBRA_CIVIL_ID != ? 
+                     AND TIPO_PROYECTO_OBRA_CIVIL_ID = ?;";
+
+        $query = $conectar->prepare($query);
+        $query->bindValue(1, $categoriaTipoProyectoObraCivilID);
+        $query->bindValue(2, $tipoProyectoObraCivilID);
+        $query->execute();
+
+        $resultado = $query->fetchAll();
+
+        return $resultado;
+    }
 }
