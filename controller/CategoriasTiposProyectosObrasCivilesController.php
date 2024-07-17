dda95ae5c0d8c59242d10b02cffc1cd844e5fe2a
andresgc04 <andresgc1997@outlook.com>
2024-07-16 23:08:06 -0400

    Modificando Modelo De CategoriasTiposProyectosObrasCiviles
    Agregando funcionalidad para obtener opciones de categorias tipos proyectos obras civiles por categoriaTipoProyectoObraCivilID y tipoProyectoObraCivilID como por igual diferente a CategoriaTipoProyectoObraCivilID y igual a tipoProyectoObraCivilID seleccionados o indicados

--- controller/CategoriasTiposProyectosObrasCivilesController.php
+++ controller/CategoriasTiposProyectosObrasCivilesController.php
@@ -23,4 +23,29 @@ switch ($_GET['op']) {
             echo $html;
         }
         break;
+    case 'obtener_listado_opciones_categorias_tipos_proyectos_obras_civiles_por_categoria_tipo_proyecto_obra_civil_ID_tipo_proyecto_obra_civil_ID':
+        $datos = $categoriasTiposProyectosObrasCiviles->obtener_listado_opciones_categorias_tipos_proyectos_obras_civiles_por_categoria_tipo_proyecto_obra_civil_ID_tipo_proyecto_obra_civil_ID(
+            $_POST['categoriaTipoProyectoObraCivilID'],
+            $_POST['tipoProyectoObraCivilID']
+        );
+
+        if (is_array($datos) == true and count($datos) > 0) {
+            foreach ($datos as $row) {
+                $html .= '<option selected value="' . $row['CATEGORIA_TIPO_PROYECTO_OBRA_CIVIL_ID'] . '">' . $row['CATEGORIA_TIPO_PROYECTO_OBRA_CIVIL'] . '</option>';
+            }
+        }
+
+        $datos2 = $categoriasTiposProyectosObrasCiviles->obtener_listado_opciones_categorias_tipos_proyectos_obras_civiles_diferente_categoria_tipo_proyecto_obra_civil_ID_tipo_proyecto_obra_civil_ID(
+            $_POST['categoriaTipoProyectoObraCivilID'],
+            $_POST['tipoProyectoObraCivilID']
+        );
+
+        if (is_array($datos2) == true and count($datos2) > 0) {
+            foreach ($datos2 as $row) {
+                $html .= '<option value="' . $row['CATEGORIA_TIPO_PROYECTO_OBRA_CIVIL_ID'] . '">' . $row['CATEGORIA_TIPO_PROYECTO_OBRA_CIVIL'] . '</option>';
+            }
+
+            echo $html;
+        }
+        break;
 }
