df8084cef9c1a87504c40413484cc131a7200fb4
andresgc04 <andresgc1997@outlook.com>
2024-07-16 22:08:20 -0400

    Modificando Controlador De TiposProyectosObrasCivilesController
    Agregando endpoint para consumir funcionalidad para obtener el listado de opciones de tipos proyectos obras civiles por tipoProyectoObraCivilID y diferente a tipoProyectoObraCivilID seleccionados o indicados.

--- controller/TiposProyectosObrasCivilesController.php
+++ controller/TiposProyectosObrasCivilesController.php
@@ -23,4 +23,23 @@ switch ($_GET['op']) {
             echo $html;
         }
         break;
+    case 'obtener_listado_opciones_tipos_proyectos_obras_civiles_por_tipo_proyecto_obra_civil_ID':
+        $datos = $tiposProyectosObrasCiviles->obtener_listado_opciones_tipos_proyectos_obras_civiles_por_tipo_proyecto_obra_civil_ID($_POST['tipoProyectoObraCivilID']);
+
+        if (is_array($datos) == true and count($datos) > 0) {
+            foreach ($datos as $row) {
+                $html .= '<option selected value="' . $row['TIPO_PROYECTO_OBRA_CIVIL_ID'] . '">' . $row['TIPO_PROYECTO_OBRA_CIVIL'] . '</option>';
+            }
+        }
+
+        $datos2 = $tiposProyectosObrasCiviles->obtener_listado_opciones_tipos_proyectos_obras_civiles_diferente_tipo_proyecto_obra_civil_ID($_POST['tipoProyectoObraCivilID']);
+
+        if (is_array($datos2) == true and count($datos2) > 0) {
+            foreach ($datos2 as $row) {
+                $html .= '<option value="' . $row['TIPO_PROYECTO_OBRA_CIVIL_ID'] . '">' . $row['TIPO_PROYECTO_OBRA_CIVIL'] . '</option>';
+            }
+
+            echo $html;
+        }
+        break;
 }
