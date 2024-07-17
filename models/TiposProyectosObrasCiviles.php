a827831731b306b262e9e451f1a6393181bae99c
andresgc04 <andresgc1997@outlook.com>
2024-07-16 21:50:37 -0400

    Modificando Modelo De TiposProyectosObrasCiviles
    Agregando funcionalidad para obtener el listado de opciones de tipos proyectos obras civiles por tipoProyectoObraCivilID y diferente al tipoProyectoObraCivilID seleccionados o indicados

--- models/TiposProyectosObrasCiviles.php
+++ models/TiposProyectosObrasCiviles.php
@@ -18,4 +18,42 @@ class TiposProyectosObrasCiviles extends Connection
 
         return $resultado;
     }
+
+    public function obtener_listado_opciones_tipos_proyectos_obras_civiles_por_tipo_proyecto_obra_civil_ID($tipoProyectoObraCivilID)
+    {
+        $conectar = parent::Connection();
+        parent::set_names();
+
+        $query = "SELECT TIPO_PROYECTO_OBRA_CIVIL_ID, 
+                         UCASE(TIPO_PROYECTO_OBRA_CIVIL) TIPO_PROYECTO_OBRA_CIVIL
+                    FROM TIPOS_PROYECTOS_OBRAS_CIVILES 
+                   WHERE TIPO_PROYECTO_OBRA_CIVIL_ID = ?;";
+
+        $query = $conectar->prepare($query);
+        $query->bindValue(1, $tipoProyectoObraCivilID);
+        $query->execute();
+
+        $resultado = $query->fetchAll();
+
+        return $resultado;
+    }
+
+    public function obtener_listado_opciones_tipos_proyectos_obras_civiles_diferente_tipo_proyecto_obra_civil_ID($tipoProyectoObraCivilID)
+    {
+        $conectar = parent::Connection();
+        parent::set_names();
+
+        $query = "SELECT TIPO_PROYECTO_OBRA_CIVIL_ID, 
+                         UCASE(TIPO_PROYECTO_OBRA_CIVIL) TIPO_PROYECTO_OBRA_CIVIL
+                    FROM TIPOS_PROYECTOS_OBRAS_CIVILES 
+                   WHERE TIPO_PROYECTO_OBRA_CIVIL_ID != ?;";
+
+        $query = $conectar->prepare($query);
+        $query->bindValue(1, $tipoProyectoObraCivilID);
+        $query->execute();
+
+        $resultado = $query->fetchAll();
+
+        return $resultado;
+    }
 }
