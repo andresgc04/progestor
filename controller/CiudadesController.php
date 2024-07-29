8d1523ea00516e93d1055431d942dc889625a498
andresgc04 <andresgc1997@outlook.com>
2024-07-28 22:08:26 -0400

    Modificando Controlador De Ciudades (CiudadesController)
    Agregando endpoint para consumir funcionalidad para obtener el detalle de las ciudades seleccionadas

--- controller/CiudadesController.php
+++ controller/CiudadesController.php
@@ -74,4 +74,40 @@ switch ($_GET["op"]) {
             echo $html;
         }
         break;
+    case 'obtener_detalles_ciudades_por_pais_ID_provincia_ID_ciudad_ID':
+        $data = $ciudades->obtener_detalles_ciudades_por_pais_ID_provincia_ID_ciudad_ID($_POST['paisID'], $_POST['provinciaID'], $_POST['ciudadID']);
+
+        if (is_array($data) == true and count($data) > 0) {
+            // Normalizar la estructura de los datos si es necesario
+            $normalizedData = array();
+
+            foreach ($data as $item) {
+                // Si los datos son un array asociativo con claves numéricas y asociativas,
+                // seleccionar las claves que deseas mantener o normalizar la estructura según sea necesario.
+                $normalizedItem = [
+                    'paisID' => $item['PAIS_ID'],
+                    'provinciaID' => $item['PROVINCIA_ID'],
+                    'ciudadID' => $item['CIUDAD_ID'],
+                    'ciudad' => $item['CIUDAD']
+                ];
+
+                // Agregar el elemento normalizado al array resultante
+                $normalizedData = $normalizedItem;
+            }
+
+            // Crear un array asociativo con la clave "data"
+            $response = array('data' => $normalizedData);
+
+            // Convertir el array de objetos a formato JSON:
+            $json = json_encode($response, JSON_UNESCAPED_UNICODE);
+
+            // Configurar la cabecera para indicar que la respuesta es JSON
+            header('Content-Type: application/json');
+
+            // Retornar o imprimir el JSON
+            echo $json;
+        } else {
+            echo json_encode(['data' => []]);
+        }
+        break;
 }
