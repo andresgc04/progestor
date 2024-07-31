154b374915e54c62a49cc6d6867ceca38b005222
andresgc04 <andresgc1997@outlook.com>
2024-07-30 23:33:15 -0400

    Modificando Formulario Modal Para Ver Detalle y Modificar Ciudades
    Agregar funcionalidad para modificar ciudades

--- view/home-cities/index.php
+++ view/home-cities/index.php
@@ -121,6 +121,7 @@ if (isset($usuarioID)) {
         <script type="text/javascript" src="../../public/js/functions/components_select_list_options/select_list_provinces_options.js"></script>
         <script type="text/javascript" src="home-cities.js"></script>
         <script type="text/javascript" src="new-cities.js"></script>
+        <script type="text/javascript" src="update-cities.js"></script>
     </body>
 
     </html>
---
+++ view/home-cities/update-cities.js
@@ -0,0 +1,83 @@
+function updateCities() {
+  let updateCitiesFormData = new FormData($("#updateCitiesForm")[0]);
+
+  $.ajax({
+    url: "../../controller/CiudadesController.php?op=modificar_ciudades_por_pais_ID_provincia_ID_ciudad_ID",
+    type: "POST",
+    data: updateCitiesFormData,
+    contentType: false,
+    processData: false,
+    success: function (data) {
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Ciudad Modificada Correctamente",
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true,
+      }).then(
+        (willClose = () => {
+          window.location.reload();
+        })
+      );
+    },
+    error: function (data) {
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "Ocurrio un error inesperado",
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true,
+      }).then(
+        (willClose = () => {
+          window.location.reload();
+        })
+      );
+    },
+  });
+}
+
+$(function () {
+  $.validator.setDefaults({
+    submitHandler: function () {
+      updateCities();
+    },
+  });
+
+  $("#updateCitiesForm").validate({
+    rules: {
+      modificarPaisID: {
+        required: true,
+      },
+      modificarProvinciaID: {
+        required: true,
+      },
+      nombreCiudad: {
+        required: true,
+      },
+    },
+    messages: {
+      modificarPaisID: {
+        required: "Por favor seleccione el país.",
+      },
+      modificarProvinciaID: {
+        required: "Por favor seleccione la provincia.",
+      },
+      modificarNombreCiudad: {
+        required: "Por favor ingrese el nombre de la ciudad.",
+      },
+    },
+    errorElement: "span",
+    errorPlacement: function (error, element) {
+      error.addClass("invalid-feedback");
+      element.closest(".form-group").append(error);
+    },
+    highlight: function (element, errorClass, validClass) {
+      $(element).addClass("is-invalid");
+    },
+    unhighlight: function (element, errorClass, validClass) {
+      $(element).removeClass("is-invalid");
+    },
+  });
+});
