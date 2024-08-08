b083c9c43fe4c85184b071af39f83beee6f553b4
andresgc04 <andresgc1997@outlook.com>
2024-08-08 15:27:01 -0400

    Agregando funcionalidad de obtener encabezado de la solicitud de proyecto en el formulario de creacion de proyectos

--- view/new-project/new-project.js
+++ view/new-project/new-project.js
@@ -9,24 +9,110 @@ setBreadCrumbContentHeaderTitle(
 
 setBreadCrumbContentHeaderSubTitle("Crear Nuevo Proyecto");
 
+const obtenerEncabezadoSolicitudesProyectosPorSolicitudProyectoID = (
+  solicitudProyectoID
+) => {
+  $.post(
+    "../../controller/SolicitudesProyectosController.php?op=obtener_encabezado_solicitudes_proyectos_por_solicitud_proyecto_ID",
+    {
+      solicitudProyectoID: solicitudProyectoID,
+    },
+    "json"
+  )
+    .done(function (data) {
+      if (data.error) {
+        Swal.fire({
+          position: "center",
+          icon: "warning",
+          title: "Ocurrio un error!!",
+          text: `${data.error}`,
+          showConfirmButton: false,
+          timer: 2000,
+          timerProgressBar: true,
+        }).then(
+          (willClose = () => {
+            window.location.reload();
+          })
+        );
+      } else {
+        const responseData = data.data;
+
+        const {
+          solicitudProyectoID,
+          nombreProyecto,
+          descripcionProyecto,
+          objetivoProyecto,
+          fechaEstimadaDeseada,
+          nombreCliente,
+        } = responseData;
+
+        const solicitudProyectoIDInput = document.getElementById(
+          "solicitudProyectoID"
+        );
+
+        const nombreProyectoInput = document.getElementById("nombreProyecto");
+
+        const descripcionProyectoInput = document.getElementById(
+          "descripcionProyecto"
+        );
+
+        const objetivoProyectoInput =
+          document.getElementById("objetivoProyecto");
+
+        const fechaEstimadaDeseadaInput = document.getElementById(
+          "fechaEstimadaDeseada"
+        );
+
+        const solicitadoPorInput = document.getElementById("solicitadoPor");
+
+        solicitudProyectoIDInput.value =
+          solicitudProyectoID != null ? solicitudProyectoID : "";
+
+        nombreProyectoInput.value =
+          nombreProyecto != null ? nombreProyecto : "";
+
+        descripcionProyectoInput.value =
+          descripcionProyecto != null ? descripcionProyecto : "";
+
+        objetivoProyectoInput.value =
+          objetivoProyecto != null ? objetivoProyecto : "";
+
+        fechaEstimadaDeseadaInput.value =
+          fechaEstimadaDeseada != null ? fechaEstimadaDeseada : "";
+
+        solicitadoPorInput.value = nombreCliente != null ? nombreCliente : "";
+      }
+    })
+    .fail(function (jqXHR, textStatus, errorThrown) {
+      Swal.fire({
+        position: "center",
+        icon: "warning",
+        title: `${textStatus}`,
+        text: `${errorThrown}`,
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true,
+      }).then(
+        (willClose = () => {
+          window.location.reload();
+        })
+      );
+    });
+};
+
 (function () {
   //GetParams:
   const solicitudProyectoID = getParams("solicitudProyectoID");
 
-  const solicitudProyectoIDInput = document.getElementById(
-    "solicitudProyectoID"
-  );
-  solicitudProyectoIDInput.value = solicitudProyectoID;
-
   //Initialize Select2 Elements:
   initializeSelect2Elements();
 
+  obtenerEncabezadoSolicitudesProyectosPorSolicitudProyectoID(
+    solicitudProyectoID
+  );
+
   //Initialize Date picker:
   $("#fechaInicioProyecto").datetimepicker({
-    format: "YYYY/MM/DD",
-  });
-
-  $("#fechaFinalizacionProyecto").datetimepicker({
     format: "YYYY/MM/DD",
   });
 
