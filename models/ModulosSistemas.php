c69894135bfd87ec400a65acdf23357e00b0bf26
andresgc04 <andresgc1997@outlook.com>
2024-08-09 23:35:41 -0400

    Modificando Modelo De Los Modulos Del Sistema
    Agregando funcionalidad para registrar nuevos modulos del sistema

--- models/ModulosSistemas.php
+++ models/ModulosSistemas.php
@@ -1,6 +1,28 @@
 <?php
 class ModulosSistemas extends Connection
 {
+    public function registrar_modulos_sistemas($moduloSistema, $creadoPor)
+    {
+        $conectar = parent::Connection();
+        parent::set_names();
+
+        $query = 'INSERT INTO MODULOS_SISTEMAS (MODULO, ESTADO_ID,
+                                                CREADO_POR, FECHA_CREACION
+                                               )
+                                         VALUES(?, 1,
+                                                ?, NOW()
+                                               );';
+
+        $query = $conectar->prepare($query);
+        $query->bindValue(1, $moduloSistema);
+        $query->bindValue(2, $creadoPor);
+        $query->execute();
+
+        $resultado = $query->fetchAll();
+
+        return $resultado;
+    }
+
     public function listado_modulos_sistemas()
     {
         $conectar = parent::Connection();
