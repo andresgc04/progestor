ecf23f626feb5a5010f9f4d6adfc599551df1d23
andresgc04 <andresgc1997@outlook.com>
2024-08-11 10:53:58 -0400

    Agregando Controlador De Accesos Modulos De Sistemas (AccesosModulosSistemasController)
    Agregando endpoint para consumir funcionalidad para obtener el listado de accesos a modulos del sistema

---
+++ controller/AccesosModulosSistemasController.php
@@ -0,0 +1,47 @@
+<?php
+require_once('../config/connection.php');
+require_once('../models/AccesosModulosSistemas.php');
+require_once("../public/php/constants/sessions-constants.php");
+
+session_start();
+
+$creadoPor = $_SESSION[$USUARIO_ID];
+$modificadoPor = $creadoPor;
+
+$accesosModulosSistemas = new AccesosModulosSistemas();
+
+switch ($_GET['op']) {
+    case "obtener_listado_accesos_modulos_sistemas":
+        $datos = $accesosModulosSistemas->obtener_listado_accesos_modulos_sistemas();
+        $data = array();
+
+        foreach ($datos as $row) {
+            $sub_array = array();
+            $sub_array[] = $row['ACCESO_MODULO_SISTEMA_ID'];
+            $sub_array[] = $row['ROL'];
+            $sub_array[] = $row['MODULO'];
+
+            if ($row["ESTADO"] === "ACTIVO") {
+                $sub_array[] = '<span class="badge badge-primary">ACTIVO</span>';
+            }
+
+            $sub_array[] = '<td class="text-right py-0 align-middle">
+                                <div class="btn-group btn-group-sm">
+                                    <button type="button" id="' . $row['ACCESO_MODULO_SISTEMA_ID'] . '" onclick="verDetalleAccesoModuloSistema(' . $row['ACCESO_MODULO_SISTEMA_ID'] . ', ' . $row['MODULO_SISTEMA_ID'] . ', ' . $row['ROL_ID'] . ')" class="btn btn-info"><i class="fas fa-eye"></i></button>
+                                    <button type="button" id="' . $row['ACCESO_MODULO_SISTEMA_ID'] . '" onclick="eliminarAccesoModuloSistema(' . $row['ACCESO_MODULO_SISTEMA_ID'] . ', ' . $row['MODULO_SISTEMA_ID'] . ', ' . $row['ROL_ID'] . ')" class="btn btn-danger"><i class="fas fa-trash"></i></button>
+                                </div>
+                            </td>';
+
+            $data[] = $sub_array;
+        }
+
+        $resultados = array(
+            "sEcho" => 1,
+            "iTotalRecords" => count($data),
+            "iTotalDisplayRecords" => count($data),
+            "aaData" => $data
+        );
+
+        echo json_encode($resultados);
+        break;
+}
