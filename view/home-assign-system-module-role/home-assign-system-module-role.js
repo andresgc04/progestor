30ea22350a221360318f964f1f2c5bce629a096a
andresgc04 <andresgc1997@outlook.com>
2024-08-11 11:25:11 -0400

    Agregando Vista De Bandeja De Entrada De Accesos A Modulos Del Sistema

---
+++ view/home-assign-system-module-role/home-assign-system-module-role.js
@@ -0,0 +1,7 @@
+navLinkPrimary("navLinkHomeAssignSystemModuleRole");
+
+setContentHeaderTitle("Listado De Accesos A Módulos Del Sistema");
+
+setBreadCrumbContentHeaderTitle("../dashboard/", "Dashboard");
+
+setBreadCrumbContentHeaderSubTitle("Listado De Accesos A Módulos Del Sistema");
