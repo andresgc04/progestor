ce97b73bf34854e9c0c815a8c2afeab72d1b5144
andresgc04 <andresgc1997@outlook.com>
2024-08-11 21:53:35 -0400

    Modificando Modelo De Modulos De Sistemas
    Agregando funcionalidad para obtener el listado de opciones de los modulos del sistema

--- models/ModulosSistemas.php
+++ models/ModulosSistemas.php
@@ -100,4 +100,23 @@ class ModulosSistemas extends Connection
 
         return $resultado;
     }
+
+    public function obtener_listado_opciones_modulos_sistemas()
+    {
+        $conectar = parent::Connection();
+        parent::set_names();
+
+        $query = 'SELECT MODULO_SISTEMA_ID,
+                         UCASE(MODULO) AS MODULO
+                    FROM MODULOS_SISTEMAS 
+                   WHERE ESTADO_ID = 1
+                ORDER BY MODULO ASC;';
+
+        $query = $conectar->prepare($query);
+        $query->execute();
+
+        $resultado = $query->fetchAll();
+
+        return $resultado;
+    }
 }
