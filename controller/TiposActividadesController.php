16df896f1870eb47896ea075d2c7e0dc8c37eed9
andresgc04 <andresgc1997@outlook.com>
2024-08-14 15:34:21 -0400

    Modificando Controlador De Tipos De Actividades (TiposActividadesController)
    Agregando endpoint para consumir funcionalidad para obtener el listado de opciones de tipos de actividades por tipoActividadID seleccionado o indicado o por igual diferente a tipoActividadID seleccionado o indicado

--- controller/TiposActividadesController.php
+++ controller/TiposActividadesController.php
@@ -24,4 +24,23 @@ switch ($_GET['op']) {
             echo $html;
         }
         break;
+    case 'obtener_listado_opciones_tipos_actividades_por_tipo_actividad_ID':
+        $datos = $tiposActividades->obtener_listado_opciones_tipos_actividades_por_tipo_actividad_ID($_POST['tipoActividadID']);
+
+        if (is_array($datos) == true and count($datos) > 0) {
+            foreach ($datos as $row) {
+                $html .= '<option selected value="' . $row['TIPO_ACTIVIDAD_ID'] . '">' . $row['TIPO_ACTIVIDAD'] . '</option>';
+            }
+        }
+
+        $datos2 = $tiposActividades->obtener_listado_opciones_tipos_actividades_diferente_tipo_actividad_ID($_POST['tipoActividadID']);
+
+        if (is_array($datos2) == true and count($datos2) > 0) {
+            foreach ($datos2 as $row) {
+                $html .= '<option value="' . $row['TIPO_ACTIVIDAD_ID'] . '">' . $row['TIPO_ACTIVIDAD'] . '</option>';
+            }
+
+            echo $html;
+        }
+        break;
 }
