3940b46f7aab54aa331caa0a6c1330a3de3e3f78
andresgc04 <andresgc1997@outlook.com>
2024-08-13 23:58:02 -0400

    Modificando Controlador De Actividades De Proyectos Obras Civiles (ActividadesProyectosObrasCivilesController)
    Agregando endpoint para consumir funcionalidad de obtener el detalle de actividades de proyectos obras civiles mediante actividadProyectoObraCivilID y proyectoObraCivilID seleccionado o indicado

--- controller/ActividadesProyectosObrasCivilesController.php
+++ controller/ActividadesProyectosObrasCivilesController.php
@@ -55,4 +55,45 @@ switch ($_GET['op']) {
 
         echo json_encode($resultados);
         break;
+    case 'obtener_detalles_actividades_proyectos_obras_civiles_por_actividad_proyecto_obra_civil_ID_proyecto_obra_civil_ID':
+        $data = $actividadesProyectosObrasCiviles->obtener_detalles_actividades_proyectos_obras_civiles_por_actividad_proyecto_obra_civil_ID_proyecto_obra_civil_ID(
+            $_POST['actividadProyectoObraCivilID'],
+            $_POST['proyectoObraCivilID']
+        );
+
+        if (is_array($data) == true and count($data) > 0) {
+            // Normalizar la estructura de los datos si es necesario
+            $normalizedData = array();
+
+            foreach ($data as $item) {
+                // Si los datos son un array asociativo con claves numéricas y asociativas,
+                // seleccionar las claves que deseas mantener o normalizar la estructura según sea necesario.
+                $normalizedItem = [
+                    'actividadID' => $item['ACTIVIDAD_ID'],
+                    'proyectoObraCivilID' => $item['PROYECTO_OBRA_CIVIL_ID'],
+                    'tipoActividadID' => $item['TIPO_ACTIVIDAD_ID'],
+                    'nombreActividad' => $item['NOMBRE_ACTIVIDAD'],
+                    'descripcionActividad' => $item['DESCRIPCION_ACTIVIDAD'],
+                    'costoActividad' => $item['COSTO_ACTIVIDAD']
+                ];
+
+                // Agregar el elemento normalizado al array resultante
+                $normalizedData = $normalizedItem;
+            }
+
+            // Crear un array asociativo con la clave "data"
+            $response = array('data' => $normalizedData);
+
+            // Convertir el array de objetos a formato JSON:
+            $json = json_encode($response, JSON_UNESCAPED_UNICODE);
+
+            // Configurar la cabecera para indicar que la respuesta es JSON
+            header('Content-Type: application/json');
+
+            // Retornar o imprimir el JSON
+            echo $json;
+        } else {
+            echo json_encode(['data' => []]);
+        }
+        break;
 }
