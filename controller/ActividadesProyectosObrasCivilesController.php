abcea3d9288954ef516a1a1ffc29de2cae0d8ee1
andresgc04 <andresgc1997@outlook.com>
2024-08-13 23:06:13 -0400

    Modificando Controlador De Actividades De Proyectos De Obras Civiles (ActividadesProyectosObrasCivilesController)
    Agregando endpoint para consumir funcionalidad de obtener el listado de actividades de proyectos de obras civiles existentes.

--- controller/ActividadesProyectosObrasCivilesController.php
+++ controller/ActividadesProyectosObrasCivilesController.php
@@ -21,4 +21,39 @@ switch ($_GET['op']) {
             $creadoPor
         );
         break;
+    case "listado_actividades_proyectos_obras_civiles":
+        $datos = $actividadesProyectosObrasCiviles->listado_actividades_proyectos_obras_civiles();
+        $data = array();
+
+        foreach ($datos as $row) {
+            $sub_array = array();
+            $sub_array[] = $row['ACTIVIDAD_ID'];
+            $sub_array[] = $row['TIPO_ACTIVIDAD'];
+            $sub_array[] = $row['NOMBRE_ACTIVIDAD'];
+            $sub_array[] = $row['DESCRIPCION_ACTIVIDAD'];
+            $sub_array[] = $row['COSTO_ACTIVIDAD'];
+
+            if ($row["ESTADO"] === "ACTIVO") {
+                $sub_array[] = '<span class="badge badge-primary">ACTIVO</span>';
+            }
+
+            $sub_array[] = '<td class="text-right py-0 align-middle">
+                                    <div class="btn-group btn-group-sm">
+                                        <button type="button" id="' . $row['ACTIVIDAD_ID'] . '" onclick="verDetalleActividadProyectoObraCivil(' . $row['ACTIVIDAD_ID'] . ', ' . $row['PROYECTO_OBRA_CIVIL_ID'] . ')" class="btn btn-info"><i class="fas fa-eye"></i></button>
+                                        <button type="button" id="' . $row['ACTIVIDAD_ID'] . '" onclick="eliminarActividadProyectoObraCivil(' . $row['ACTIVIDAD_ID'] . ', ' . $row['PROYECTO_OBRA_CIVIL_ID'] . ')" class="btn btn-danger"><i class="fas fa-trash"></i></button>
+                                    </div>
+                                </td>';
+
+            $data[] = $sub_array;
+        }
+
+        $resultados = array(
+            "sEcho" => 1,
+            "iTotalRecords" => count($data),
+            "iTotalDisplayRecords" => count($data),
+            "aaData" => $data
+        );
+
+        echo json_encode($resultados);
+        break;
 }
