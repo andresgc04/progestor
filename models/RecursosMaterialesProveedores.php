4e503d50e82fa4a8405ae045f0ac3a321d194c43
andresgc04 <andresgc1997@outlook.com>
2024-08-17 17:19:30 -0400

    Modificando Modelo De Recursos Materiales Proveedor
    Agregando funcionalidad para registrar nuevos recursos materiales por proveedores

--- models/RecursosMaterialesProveedores.php
+++ models/RecursosMaterialesProveedores.php
@@ -1,6 +1,30 @@
 <?php
 class RecursosMaterialesProveedores extends Connection
 {
+    public function registrar_recursos_materiales_proveedores($recursoMaterialID, $proveedorID, $costoRecursoMaterial, $creadoPor)
+    {
+        $conectar = parent::Connection();
+        parent::set_names();
+
+        $query = 'INSERT INTO RECURSOS_MATERIALES_PROVEEDORES (RECURSO_MATERIAL_ID, PROVEEDOR_ID, COSTO_RECURSO_MATERIAL,
+                                                               ESTADO_ID, CREADO_POR, FECHA_CREACION
+                                                              )
+                                                        VALUES(?, ?, ?,
+                                                               1, ?, NOW()
+                                                              );';
+
+        $query = $conectar->prepare($query);
+        $query->bindValue(1, $recursoMaterialID);
+        $query->bindValue(2, $proveedorID);
+        $query->bindValue(3, $costoRecursoMaterial);
+        $query->bindValue(4, $creadoPor);
+        $query->execute();
+
+        $resultado = $query->fetchAll();
+
+        return $resultado;
+    }
+
     public function listado_recursos_materiales_proveedores()
     {
         $conectar = parent::Connection();
