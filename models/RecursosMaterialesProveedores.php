56b461f71762fe2e9ebc74008c88bb6219f52a6d
andresgc04 <andresgc1997@outlook.com>
2024-08-18 19:01:44 -0400

    Modificando Modelo De RecursosMaterialesProveedores
    Agregando funcionalidad para obtener el detalle de los recursos materiales proveedores por recursoMaterialID y proveedorID seleccionados

--- models/RecursosMaterialesProveedores.php
+++ models/RecursosMaterialesProveedores.php
@@ -1,36 +1,36 @@
 <?php
 class RecursosMaterialesProveedores extends Connection
 {
-    public function registrar_recursos_materiales_proveedores($recursoMaterialID, $proveedorID, $costoRecursoMaterial, $creadoPor)
-    {
-        $conectar = parent::Connection();
-        parent::set_names();
+        public function registrar_recursos_materiales_proveedores($recursoMaterialID, $proveedorID, $costoRecursoMaterial, $creadoPor)
+        {
+                $conectar = parent::Connection();
+                parent::set_names();
 
-        $query = 'INSERT INTO RECURSOS_MATERIALES_PROVEEDORES (RECURSO_MATERIAL_ID, PROVEEDOR_ID, COSTO_RECURSO_MATERIAL,
+                $query = 'INSERT INTO RECURSOS_MATERIALES_PROVEEDORES (RECURSO_MATERIAL_ID, PROVEEDOR_ID, COSTO_RECURSO_MATERIAL,
                                                                ESTADO_ID, CREADO_POR, FECHA_CREACION
                                                               )
                                                         VALUES(?, ?, ?,
                                                                1, ?, NOW()
                                                               );';
 
-        $query = $conectar->prepare($query);
-        $query->bindValue(1, $recursoMaterialID);
-        $query->bindValue(2, $proveedorID);
-        $query->bindValue(3, $costoRecursoMaterial);
-        $query->bindValue(4, $creadoPor);
-        $query->execute();
+                $query = $conectar->prepare($query);
+                $query->bindValue(1, $recursoMaterialID);
+                $query->bindValue(2, $proveedorID);
+                $query->bindValue(3, $costoRecursoMaterial);
+                $query->bindValue(4, $creadoPor);
+                $query->execute();
 
-        $resultado = $query->fetchAll();
+                $resultado = $query->fetchAll();
 
-        return $resultado;
-    }
+                return $resultado;
+        }
 
-    public function listado_recursos_materiales_proveedores()
-    {
-        $conectar = parent::Connection();
-        parent::set_names();
+        public function listado_recursos_materiales_proveedores()
+        {
+                $conectar = parent::Connection();
+                parent::set_names();
 
-        $query = 'SELECT recursosMaterialesProveedores.RECURSO_MATERIAL_ID,
+                $query = 'SELECT recursosMaterialesProveedores.RECURSO_MATERIAL_ID,
                          recursosMaterialesProveedores.PROVEEDOR_ID,
                          UCASE(tiposRecursosMateriales.TIPO_RECURSO_MATERIAL) AS TIPO_RECURSO_MATERIAL,
                          UCASE(recursosMateriales.RECURSO_MATERIAL) AS RECURSO_MATERIAL,
@@ -49,11 +49,35 @@ class RecursosMaterialesProveedores extends Connection
                    WHERE recursosMaterialesProveedores.ESTADO_ID = 1
                 ORDER BY recursosMaterialesProveedores.FECHA_CREACION DESC;';
 
-        $query = $conectar->prepare($query);
-        $query->execute();
+                $query = $conectar->prepare($query);
+                $query->execute();
 
-        $resultado = $query->fetchAll();
+                $resultado = $query->fetchAll();
 
-        return $resultado;
-    }
+                return $resultado;
+        }
+
+        public function obtener_detalles_recursos_materiales_proveedores_por_recurso_material_ID_proveedor_ID($recursoMaterialID, $proveedorID)
+        {
+                $conectar = parent::Connection();
+                parent::set_names();
+
+                $query = 'SELECT recursosMateriales.TIPO_RECURSO_MATERIAL_ID,
+    	                         recursosMaterialesProveedores.RECURSO_MATERIAL_ID,
+                                 recursosMaterialesProveedores.PROVEEDOR_ID,
+                                 recursosMaterialesProveedores.COSTO_RECURSO_MATERIAL
+                            FROM RECURSOS_MATERIALES_PROVEEDORES recursosMaterialesProveedores
+                      INNER JOIN RECURSOS_MATERIALES recursosMateriales 
+                              ON recursosMaterialesProveedores.RECURSO_MATERIAL_ID = recursosMateriales.RECURSO_MATERIAL_ID
+	                   WHERE recursosMaterialesProveedores.RECURSO_MATERIAL_ID = ? AND recursosMaterialesProveedores.PROVEEDOR_ID = ?;';
+
+                $query = $conectar->prepare($query);
+                $query->bindValue(1, $recursoMaterialID);
+                $query->bindValue(2, $proveedorID);
+                $query->execute();
+
+                $resultado = $query->fetchAll();
+
+                return $resultado;
+        }
 }
