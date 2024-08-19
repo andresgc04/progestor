6efd76f2f434ddac888f504cdccae687e557373a
andresgc04 <andresgc1997@outlook.com>
2024-08-18 21:07:05 -0400

    Agregando componente del select list para proveedores por proveedorID y diferente a proveedorID seleccioando o indicado

--- public/js/functions/components_select_list_options/select_list_suppliers_options.js
+++ public/js/functions/components_select_list_options/select_list_suppliers_options.js
@@ -18,3 +18,28 @@ const getSelectListSuppliersOptions = (urlController, inputFieldID) => {
       );
     });
 };
+
+const getSelectListSuppliersOptionsByProveedorID = (
+  urlController,
+  proveedorID,
+  inputFieldID
+) => {
+  $.post(urlController, { proveedorID: proveedorID })
+    .done(function (data, status) {
+      $(inputFieldID).html(data);
+    })
+    .fail(function (data, status) {
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "Ocurrio un error inesperado",
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true,
+      }).then(
+        (willClose = () => {
+          window.location.reload();
+        })
+      );
+    });
+};
