0924781d858d0a00fed0c7e807e08baac3e792cb
andresgc04 <andresgc1997@outlook.com>
2024-08-20 16:37:48 -0400

    Agregando Controlador De Unidades De Medidas (UnidadesMedidasController)
    Agregando endpoint para consumir la funcionalidad del listado de unidades de medidas existentes

--- models/UnidadesMedidas.php
+++ models/UnidadesMedidas.php
@@ -6,11 +6,14 @@ class UnidadesMedidas extends Connection
         $conectar = parent::Connection();
         parent::set_names();
 
-        $query = 'SELECT UNIDAD_MEDIDA_ID,
-	                     UCASE(UNIDAD_MEDIDA) AS UNIDAD_MEDIDA
-                    FROM UNIDADES_MEDIDAS 
-                   WHERE ESTADO_ID = 1
-                ORDER BY UNIDAD_MEDIDA_ID DESC, FECHA_CREACION DESC;';
+        $query = 'SELECT unidadesMedidas.UNIDAD_MEDIDA_ID,
+	                     UCASE(unidadesMedidas.UNIDAD_MEDIDA) AS UNIDAD_MEDIDA,
+                         UCASE(estados.ESTADO) AS ESTADOS
+                    FROM UNIDADES_MEDIDAS unidadesMedidas
+              INNER JOIN ESTADOS estados
+		              ON unidadesMedidas.ESTADO_ID = estados.ESTADO_ID
+                   WHERE unidadesMedidas.ESTADO_ID = 1
+                ORDER BY unidadesMedidas.UNIDAD_MEDIDA_ID DESC, unidadesMedidas.FECHA_CREACION DESC;';
 
         $query = $conectar->prepare($query);
         $query->execute();
