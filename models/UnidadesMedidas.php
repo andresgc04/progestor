7630f22864fb851472864d68085b776ab7ff12ea
andresgc04 <andresgc1997@outlook.com>
2024-08-20 23:05:18 -0400

    Modificando Modelo De Unidades De Medidas
    Agregando funcionalidad para registrar unidades de medidas

--- models/UnidadesMedidas.php
+++ models/UnidadesMedidas.php
@@ -1,6 +1,27 @@
 <?php
 class UnidadesMedidas extends Connection
 {
+    public function registrar_unidades_medidas($unidadMedida, $creadoPor)
+    {
+        $conectar = parent::Connection();
+        parent::set_names();
+
+        $query = 'INSERT INTO UNIDADES_MEDIDAS (UNIDAD_MEDIDA, ESTADO_ID,
+                                                CREADO_POR, FECHA_CREACION
+                                               )
+                                         VALUES(?, 1,
+                                                ?, NOW());';
+
+        $query = $conectar->prepare($query);
+        $query->bindValue(1, $unidadMedida);
+        $query->bindValue(2, $creadoPor);
+        $query->execute();
+
+        $resultado = $query->fetchAll();
+
+        return $resultado;
+    }
+
     public function listado_unidades_medidas()
     {
         $conectar = parent::Connection();
