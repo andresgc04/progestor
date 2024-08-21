60b129652d7eabfb2d362c548674167d248736ad
andresgc04 <andresgc1997@outlook.com>
2024-08-21 08:54:59 -0400

    Modificando Modelo De Unidad De Medida
    Agregando Funcionalidad De Obtener El Listado De Opciones De Unidades De Medidas

--- models/UnidadesMedidas.php
+++ models/UnidadesMedidas.php
@@ -108,4 +108,23 @@ class UnidadesMedidas extends Connection
 
         return $resultado;
     }
+
+    public function obtener_listado_opciones_unidades_medidas()
+    {
+        $conectar = parent::Connection();
+        parent::set_names();
+
+        $query = 'SELECT UNIDAD_MEDIDA_ID,
+                         UCASE(UNIDAD_MEDIDA) AS UNIDAD_MEDIDA
+                    FROM UNIDADES_MEDIDAS
+                   WHERE ESTADO_ID = 1
+                ORDER BY UNIDAD_MEDIDA ASC;';
+
+        $query = $conectar->prepare($query);
+        $query->execute();
+
+        $resultado = $query->fetchAll();
+
+        return $resultado;
+    }
 }
