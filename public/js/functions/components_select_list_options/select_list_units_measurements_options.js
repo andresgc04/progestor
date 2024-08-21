06f78ec6b102d9600836e8f585fb709963f853bc
andresgc04 <andresgc1997@outlook.com>
2024-08-21 10:07:00 -0400

    Agregando funcionalidad de componente del select de las opciones de unidades de medidas seleccionado por unidadMedidaID o diferente unidadMedidaID

--- public/js/functions/components_select_list_options/select_list_units_measurements_options.js
+++ public/js/functions/components_select_list_options/select_list_units_measurements_options.js
@@ -18,3 +18,28 @@ const getSelectListUnitsMeasurementsOptions = (urlController, inputFieldID) => {
       );
     });
 };
+
+const getSelectListUnitsMeasurementsOptionsByUnidadMedidaID = (
+  urlController,
+  unidadMedidaID,
+  inputFieldID
+) => {
+  $.post(urlController, { unidadMedidaID: unidadMedidaID })
+    .done(function (data, status) {
+      $(inputFieldID).html(data);
+    })
+    .fail(function (data, status) {
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "Ocurrio un error inesperado",
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true,
+      }).then(
+        (willClose = () => {
+          window.location.reload();
+        })
+      );
+    });
+};
