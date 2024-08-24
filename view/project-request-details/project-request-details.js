86eb5c4b212c73731bd5e30634f34041c568b93b
andresgc04 <andresgc1997@outlook.com>
2024-08-24 01:38:38 -0400

    Agregando funcionalidad para mostrar los documentos de la solicitud de proyecto

--- view/project-request-details/project-request-details.js
+++ view/project-request-details/project-request-details.js
@@ -240,7 +240,7 @@ const obtenerRequerimientosSolicitudesProyectosPorSolicitudProyectoIDDataTable =
         bDestroy: true,
         responsive: true,
         bInfo: true,
-        iDisplayLength: 10,
+        iDisplayLength: 5,
         autoWidth: false,
         language: {
           sProcessing: "Procesando...",
@@ -272,6 +272,63 @@ const obtenerRequerimientosSolicitudesProyectosPorSolicitudProyectoIDDataTable =
       .DataTable();
   };
 
+const obtenerDocumentosSolicitudesProyectosPorSolicitudProyectoIDDataTable = (
+  solicitudProyectoID
+) => {
+  $("#listadoDocumentosSolicitudesProyectosDataTable")
+    .dataTable({
+      aProcessing: true,
+      aServerSide: true,
+      dom: "Bfrtip",
+      searching: true,
+      lengthChange: false,
+      colReorder: true,
+      buttons: ["copyHtml5", "excelHtml5", "csvHtml5", "pdfHtml5"],
+      ajax: {
+        url: "../../controller/SolicitudesProyectosController.php?op=obtener_documentos_solicitudes_proyectos_por_solicitud_proyecto_ID",
+        type: "post",
+        dataType: "json",
+        data: { solicitudProyectoID: solicitudProyectoID },
+        error: function (e) {
+          console.log(e.responseText);
+        },
+      },
+      ordering: false,
+      bDestroy: true,
+      responsive: true,
+      bInfo: true,
+      iDisplayLength: 5,
+      autoWidth: false,
+      language: {
+        sProcessing: "Procesando...",
+        sLengthMenu: "Mostrar _MENU_ registros",
+        sZeroRecords: "No se encontraron resultados",
+        sEmptyTable: "Ningun dato disponible en esta tabla",
+        sInfo: "Mostrando un total de _TOTAL_ registros",
+        sInfoEmpty: "Mostrando un total de 0 registros",
+        sInfoFiltered: "(filtrado de un total de _MAX_ registros)",
+        sInfoPostFix: "",
+        sSearch: "Buscar:",
+        sUrl: "",
+        sInfoThousands: ",",
+        sLoadingRecords: "Cargando....",
+        oPaginate: {
+          sFirst: "Primero",
+          sLast: "Ultimo",
+          sNext: "Siguiente",
+          sPrevious: "Anterior",
+        },
+        oAria: {
+          sSortAscending:
+            ": Activar para ordenar la columna de manera ascendente",
+          sSortDescending:
+            ": Activar para ordenar la columna de manera descendente",
+        },
+      },
+    })
+    .DataTable();
+};
+
 (function () {
   //Initialize Date picker:
   $("#fechaEstimadaDeseada").datetimepicker({
@@ -285,6 +342,10 @@ const obtenerRequerimientosSolicitudesProyectosPorSolicitudProyectoIDDataTable =
   );
 
   obtenerRequerimientosSolicitudesProyectosPorSolicitudProyectoIDDataTable(
+    solicitudProyectoID
+  );
+
+  obtenerDocumentosSolicitudesProyectosPorSolicitudProyectoIDDataTable(
     solicitudProyectoID
   );
 })();
