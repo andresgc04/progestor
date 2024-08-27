cccfa0d12fb4f752ddb2f14e3752d0f2c5c51803
andresgc04 <andresgc1997@outlook.com>
2024-08-27 16:32:20 -0400

    Agregando funcionalidad para mostrar listado de recursos materiales por proyectos

--- view/details-civil-works-projects/details-civil-works-projects.js
+++ view/details-civil-works-projects/details-civil-works-projects.js
@@ -255,6 +255,63 @@ const obtenerListadoActividadesProyectosObrasCivilesDataTable = (
     .DataTable();
 };
 
+const obtenerListadoRecursosMaterialesProyectosObrasCivilesDataTable = (
+  proyectoObraCivilID
+) => {
+  $("#listadoRecursosMaterialesProyectosObrasCivilesDataTable")
+    .dataTable({
+      aProcessing: true,
+      aServerSide: true,
+      dom: "Bfrtip",
+      searching: true,
+      lengthChange: false,
+      colReorder: true,
+      buttons: ["copyHtml5", "excelHtml5", "csvHtml5", "pdfHtml5"],
+      ajax: {
+        url: "../../controller/RecursosMaterialesProyectosObrasCivilesController.php?op=listado_recursos_materiales_proyectos_obras_civiles_por_proyecto_obra_civil_ID",
+        type: "post",
+        dataType: "json",
+        data: { proyectoObraCivilID: proyectoObraCivilID },
+        error: function (e) {
+          console.log(e.responseText);
+        },
+      },
+      ordering: false,
+      bDestroy: true,
+      responsive: true,
+      bInfo: true,
+      iDisplayLength: 10,
+      autoWidth: false,
+      language: {
+        sProcessing: "Procesando...",
+        sLengthMenu: "Mostrar _MENU_ registros",
+        sZeroRecords: "No se encontraron resultados",
+        sEmptyTable: "Ningun dato disponible en esta tabla",
+        sInfo: "Mostrando un total de _TOTAL_ registros",
+        sInfoEmpty: "Mostrando un total de 0 registros",
+        sInfoFiltered: "(filtrado de un total de _MAX_ registros)",
+        sInfoPostFix: "",
+        sSearch: "Buscar:",
+        sUrl: "",
+        sInfoThousands: ",",
+        sLoadingRecords: "Cargando....",
+        oPaginate: {
+          sFirst: "Primero",
+          sLast: "Ultimo",
+          sNext: "Siguiente",
+          sPrevious: "Anterior",
+        },
+        oAria: {
+          sSortAscending:
+            ": Activar para ordenar la columna de manera ascendente",
+          sSortDescending:
+            ": Activar para ordenar la columna de manera descendente",
+        },
+      },
+    })
+    .DataTable();
+};
+
 const obtenerListadoDocumentosProyectosObrasCivilesDataTable = (
   solicitudProyectoID,
   proyectoObraCivilID
@@ -339,6 +396,10 @@ const obtenerListadoDocumentosProyectosObrasCivilesDataTable = (
   );
 
   obtenerListadoActividadesProyectosObrasCivilesDataTable(proyectoObraCivilID);
+
+  obtenerListadoRecursosMaterialesProyectosObrasCivilesDataTable(
+    proyectoObraCivilID
+  );
 
   obtenerListadoDocumentosProyectosObrasCivilesDataTable(
     solicitudProyectoID,
