7c4bfc7b016c4f10c0a51695c9657794c89c6d17
andresgc04 <andresgc1997@outlook.com>
2024-08-27 14:52:59 -0400

    Agregando controlador de recursos materiales proyectos obras civiles (RecursosMaterialesProyectosObrasCivilesController)
    Agregando endpoint para consumir funcionalidad para el registro de recursos materiales de proyectos de obras civiles

---
+++ controller/RecursosMaterialesProyectosObrasCivilesController.php
@@ -0,0 +1,30 @@
+<?php
+require_once('../config/connection.php');
+require_once('../models/RecursosMaterialesProyectosObrasCiviles.php');
+require_once('../public/php/constants/sessions-constants.php');
+
+session_start();
+
+$creadoPor = $_SESSION[$USUARIO_ID];
+$modificadoPor = $creadoPor;
+
+$recursosMaterialesProyectosObrasCiviles = new RecursosMaterialesProyectosObrasCiviles();
+
+switch ($_GET['op']) {
+    case "registrar_actividades_proyectos_obras_civiles":
+        $recursosMaterialesProyectosObrasCiviles->registrar_recursos_materiales_proyectos_obras_civiles(
+            $_POST["addResourceMaterialProyectoObraCivilID"],
+            $_POST['faseProyectoID'],
+            $_POST['proveedorID'],
+            $_POST['tipoRecursoMaterialID'],
+            $_POST['recursoMaterialID'],
+            $_POST['unidadMedidaRecursoMaterial'],
+            $_POST['cantidadRecursosMateriales'],
+            $_POST['costoRecursoMaterial'],
+            $_POST['subTotalRecursoMaterial'],
+            $_POST['itbisRecursoMaterial'],
+            $_POST['costoTotalRecursoMaterial'],
+            $creadoPor
+        );
+        break;
+}
