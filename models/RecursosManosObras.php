25c2bca2c38fee184ab3af09e48020e1d448992e
andresgc04 <andresgc1997@outlook.com>
2024-09-01 20:36:58 -0400

    Modificando Modelo De Recursos Manos De Obras
    Agregando funcionalidad para obtene el listado de recursos de manos de obras existentes

--- models/RecursosManosObras.php
+++ models/RecursosManosObras.php
@@ -14,7 +14,7 @@ class RecursosManosObras extends Connection
                                                     COSTO_PAGO_RECURSO_MANO_OBRA, ESTADO_ID,
                                                     CREADO_POR, FECHA_CREACION
                                                    )
-                                             VALUES(?, ?,
+                                             VALUES(UCASE(?), ?,
                                                     ?, 1,
                                                     ?, NOW()
                                                    );';
@@ -24,6 +24,33 @@ class RecursosManosObras extends Connection
         $query->bindValue(2, $tipoPagoID);
         $query->bindValue(3, $costoPagoRecursoManoObra);
         $query->bindValue(4, $creadoPor);
+        $query->execute();
+
+        $resultado = $query->fetchAll();
+
+        return $resultado;
+    }
+
+    public function listado_recursos_manos_obras()
+    {
+        $conectar = parent::Connection();
+        parent::set_names();
+
+        $query = 'SELECT recursosManosObras.RECURSO_MANO_OBRA_ID,
+    	                 UCASE(recursosManosObras.RECURSO_MANO_OBRA) AS RECURSO_MANO_OBRA,
+                         UCASE(tiposPagos.TIPO_PAGO) AS TIPO_PAGO,
+                         recursosManosObras.COSTO_PAGO_RECURSO_MANO_OBRA,
+                         UCASE(estados.ESTADO) AS ESTADO
+                    FROM RECURSOS_MANOS_OBRAS recursosManosObras
+              INNER JOIN TIPOS_PAGOS tiposPagos
+                      ON recursosManosObras.TIPO_PAGO_ID = tiposPagos.TIPO_PAGO_ID
+              INNER JOIN ESTADOS estados
+                      ON recursosManosObras.ESTADO_ID = estados.ESTADO_ID
+                   WHERE recursosManosObras.ESTADO_ID = 1
+                ORDER BY recursosManosObras.RECURSO_MANO_OBRA_ID DESC, 
+   		                 recursosManosObras.FECHA_CREACION DESC;';
+
+        $query = $conectar->prepare($query);
         $query->execute();
 
         $resultado = $query->fetchAll();
