24a43eac28b9cf07a0ab30aaeef99861fbf09732
andresgc04 <andresgc1997@outlook.com>
2024-09-01 21:45:10 -0400

    Agregando funcionalidad para obtener y mostras el listado de Recursos De Manos De Obras Existentes

--- view/home-labor-resources/home-labor-resources.js
+++ view/home-labor-resources/home-labor-resources.js
@@ -7,3 +7,64 @@ setContentHeaderTitle("Listado De Recursos De Manos De Obras");
 setBreadCrumbContentHeaderTitle("../dashboard/", "Dashboard");
 
 setBreadCrumbContentHeaderSubTitle("Listado De Recursos De Manos De Obras");
+
+const obtenerListadoRecursosManosObrasDataTable = () => {
+  $("#listadoRecursosManosObrasDataTable")
+    .dataTable({
+      aProcessing: true,
+      aServerSide: true,
+      dom: "Bfrtip",
+      searching: true,
+      lengthChange: false,
+      colReorder: true,
+      buttons: ["copyHtml5", "excelHtml5", "csvHtml5", "pdfHtml5"],
+      ajax: {
+        url: "../../controller/RecursosManosObrasController.php?op=listado_recursos_manos_obras",
+        type: "post",
+        dataType: "json",
+        error: function (e) {
+          console.log(e.responseText);
+        },
+      },
+      ordering: false,
+      bDestroy: true,
+      responsive: true,
+      bInfo: true,
+      iDisplayLength: 10,
+      autoWidth: false,
+      language: {
+        sProcessing: "Procesando...",
+        sLengthMenu: "Mostrar _MENU_ registros",
+        sZeroRecords: "No se encontraron resultados",
+        sEmptyTable: "Ningun dato disponible en esta tabla",
+        sInfo: "Mostrando un total de _TOTAL_ registros",
+        sInfoEmpty: "Mostrando un total de 0 registros",
+        sInfoFiltered: "(filtrado de un total de _MAX_ registros)",
+        sInfoPostFix: "",
+        sSearch: "Buscar:",
+        sUrl: "",
+        sInfoThousands: ",",
+        sLoadingRecords: "Cargando....",
+        oPaginate: {
+          sFirst: "Primero",
+          sLast: "Ultimo",
+          sNext: "Siguiente",
+          sPrevious: "Anterior",
+        },
+        oAria: {
+          sSortAscending:
+            ": Activar para ordenar la columna de manera ascendente",
+          sSortDescending:
+            ": Activar para ordenar la columna de manera descendente",
+        },
+      },
+    })
+    .DataTable();
+};
+
+(function () {
+  //Initialize Select2 Elements:
+  initializeSelect2Elements();
+
+  obtenerListadoRecursosManosObrasDataTable();
+})();
