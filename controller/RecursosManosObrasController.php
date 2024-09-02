5194787d9e4a15931e8ccdc226d65de9636d4811
andresgc04 <andresgc1997@outlook.com>
2024-09-01 21:03:50 -0400

    Modificando Controlador De Recursos De Manos De Obras (RecursosManosObrasController)
    Agregando endpoint para consumir funcionalidad para obtener el listado de recursos de manos de obras existentes

--- controller/RecursosManosObrasController.php
+++ controller/RecursosManosObrasController.php
@@ -11,12 +11,46 @@ $modificadoPor = $creadoPor;
 $recursosManosObras = new RecursosManosObras();
 
 switch ($_GET['op']) {
-    case "registrar_tipos_pagos":
+    case "registrar_recursos_manos_obras":
         $recursosManosObras->registrar_recursos_manos_obras(
             $_POST['recursoManoObra'],
             $_POST['tipoPagoID'],
             $_POST['costoPagoRecursoManoObra'],
             $creadoPor
         );
+        break;
+    case "listado_recursos_manos_obras":
+        $datos = $recursosManosObras->listado_recursos_manos_obras();
+        $data = array();
+
+        foreach ($datos as $row) {
+            $sub_array = array();
+            $sub_array[] = $row['RECURSO_MANO_OBRA_ID'];
+            $sub_array[] = $row['RECURSO_MANO_OBRA'];
+            $sub_array[] = $row['TIPO_PAGO'];
+            $sub_array[] = $row['COSTO_PAGO_RECURSO_MANO_OBRA'];
+
+            if ($row["ESTADO"] === "ACTIVO") {
+                $sub_array[] = '<span class="badge badge-primary">ACTIVO</span>';
+            }
+
+            $sub_array[] = '<td class="text-right py-0 align-middle">
+                                        <div class="btn-group btn-group-sm">
+                                            <button type="button" id="' . $row['RECURSO_MANO_OBRA_ID'] . '" onclick="verDetalleRecursoManoObra(' . $row['RECURSO_MANO_OBRA_ID'] . ')" class="btn btn-info"><i class="fas fa-eye"></i></button>
+                                            <button type="button" id="' . $row['RECURSO_MANO_OBRA_ID'] . '" onclick="eliminarRecursoManoObra(' . $row['RECURSO_MANO_OBRA_ID'] . ')"class="btn btn-danger"><i class="fas fa-trash"></i></button>
+                                        </div>
+                                    </td>';
+
+            $data[] = $sub_array;
+        }
+
+        $resultados = array(
+            "sEcho" => 1,
+            "iTotalRecords" => count($data),
+            "iTotalDisplayRecords" => count($data),
+            "aaData" => $data
+        );
+
+        echo json_encode($resultados);
         break;
 }
