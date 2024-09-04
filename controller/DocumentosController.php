596323ddb897335743213edfa3d4fc0417fb5051
andresgc04 <andresgc1997@outlook.com>
2024-09-04 00:13:46 -0400

    Agregando endpoints para obtener rutas de los documentos

--- controller/DocumentosController.php
+++ controller/DocumentosController.php
@@ -18,4 +18,76 @@ switch ($_GET['op']) {
             $creadoPor
         );
         break;
+    case 'obtener_ruta_documento_proyecto_obra_civil_por_documento_ID_solicitud_proyecto_ID':
+        $data = $documentos->obtener_ruta_documento_proyecto_obra_civil_por_documento_ID_solicitud_proyecto_ID(
+            $_POST['documentoID'],
+            $_POST['solicitudProyectoID'],
+        );
+
+        if (is_array($data) == true and count($data) > 0) {
+            // Normalizar la estructura de los datos si es necesario
+            $normalizedData = array();
+
+            foreach ($data as $item) {
+                // Si los datos son un array asociativo con claves numéricas y asociativas,
+                // seleccionar las claves que deseas mantener o normalizar la estructura según sea necesario.
+                $normalizedItem = [
+                    'nombreDocumento' => $item['NOMBRE_DOCUMENTO'],
+                ];
+
+                // Agregar el elemento normalizado al array resultante
+                $normalizedData = $normalizedItem;
+            }
+
+            // Crear un array asociativo con la clave "data"
+            $response = array('data' => $normalizedData);
+
+            // Convertir el array de objetos a formato JSON:
+            $json = json_encode($response, JSON_UNESCAPED_UNICODE);
+
+            // Configurar la cabecera para indicar que la respuesta es JSON
+            header('Content-Type: application/json');
+
+            // Retornar o imprimir el JSON
+            echo $json;
+        } else {
+            echo json_encode(['data' => []]);
+        }
+        break;
+    case 'obtener_ruta_documento_proyecto_obra_civil_por_documento_ID_proyecto_obra_civil_ID':
+        $data = $documentos->obtener_ruta_documento_proyecto_obra_civil_por_documento_ID_proyecto_obra_civil_ID(
+            $_POST['documentoID'],
+            $_POST['proyectoObraCivilID'],
+        );
+
+        if (is_array($data) == true and count($data) > 0) {
+            // Normalizar la estructura de los datos si es necesario
+            $normalizedData = array();
+
+            foreach ($data as $item) {
+                // Si los datos son un array asociativo con claves numéricas y asociativas,
+                // seleccionar las claves que deseas mantener o normalizar la estructura según sea necesario.
+                $normalizedItem = [
+                    'nombreDocumento' => $item['NOMBRE_DOCUMENTO'],
+                ];
+
+                // Agregar el elemento normalizado al array resultante
+                $normalizedData = $normalizedItem;
+            }
+
+            // Crear un array asociativo con la clave "data"
+            $response = array('data' => $normalizedData);
+
+            // Convertir el array de objetos a formato JSON:
+            $json = json_encode($response, JSON_UNESCAPED_UNICODE);
+
+            // Configurar la cabecera para indicar que la respuesta es JSON
+            header('Content-Type: application/json');
+
+            // Retornar o imprimir el JSON
+            echo $json;
+        } else {
+            echo json_encode(['data' => []]);
+        }
+        break;
 }
