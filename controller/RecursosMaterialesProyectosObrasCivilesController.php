45b8a15d30461d7e9bf4a160c9e9e3033b22348e
andresgc04 <andresgc1997@outlook.com>
2024-09-05 01:08:38 -0400

    Agregando funcionalidad de obtener el listado de recursos materiales

--- controller/RecursosMaterialesProyectosObrasCivilesController.php
+++ controller/RecursosMaterialesProyectosObrasCivilesController.php
@@ -62,6 +62,40 @@ switch ($_GET['op']) {
 
         echo json_encode($resultados);
         break;
+    case "listado_recursos_materiales_proyectos_obras_civiles_clientes_por_proyecto_obra_civil_ID":
+        $datos = $recursosMaterialesProyectosObrasCiviles->listado_recursos_materiales_proyectos_obras_civiles_por_proyecto_obra_civil_ID($_POST['proyectoObraCivilID']);
+        $data = array();
+
+        foreach ($datos as $row) {
+            $sub_array = array();
+            $sub_array[] = $row['RECURSO_MATERIAL_PROYECTO_OBRA_CIVIL_ID'];
+            $sub_array[] = $row['FASE_PROYECTO'];
+            $sub_array[] = $row['TIPO_RECURSO_MATERIAL'];
+            $sub_array[] = $row['RECURSO_MATERIAL'];
+            $sub_array[] = "RD$ " . number_format($row['COSTO_TOTAL'], 2, '.', ',');
+
+            if ($row["ESTADOS"] === "ACTIVO") {
+                $sub_array[] = '<span class="badge badge-primary">ACTIVO</span>';
+            }
+
+            $sub_array[] = '<td class="text-right py-0 align-middle">
+                                            <div class="btn-group btn-group-sm">
+                                                <button type="button" id="' . $row['RECURSO_MATERIAL_PROYECTO_OBRA_CIVIL_ID'] . '" onclick="verDetalleRecursoMaterialProyectoObraCivil(' . $row['RECURSO_MATERIAL_PROYECTO_OBRA_CIVIL_ID'] . ', ' . $row['PROYECTO_OBRA_CIVIL_ID'] . ')" class="btn btn-info"><i class="fas fa-eye"></i></button>
+                                            </div>
+                                        </td>';
+
+            $data[] = $sub_array;
+        }
+
+        $resultados = array(
+            "sEcho" => 1,
+            "iTotalRecords" => count($data),
+            "iTotalDisplayRecords" => count($data),
+            "aaData" => $data
+        );
+
+        echo json_encode($resultados);
+        break;
     case 'obtener_costos_totales_recursos_materiales_proyectos_obras_civiles_por_proyecto_obra_civil_ID':
         $data = $recursosMaterialesProyectosObrasCiviles->obtener_costos_totales_recursos_materiales_proyectos_obras_civiles_por_proyecto_obra_civil_ID(
             $_POST['proyectoObraCivilID']
--- view/details-clients-civil-works-projects/details-clients-civil-works-projects.js
+++ view/details-clients-civil-works-projects/details-clients-civil-works-projects.js
@@ -267,6 +267,63 @@ const obtenerListadoActividadesProyectosObrasCivilesClientesDataTable = (
     .DataTable();
 };
 
+const obtenerListadoRecursosMaterialesProyectosObrasCivilesClientesDataTable = (
+  proyectoObraCivilID
+) => {
+  $("#listadoRecursosMaterialesProyectosObrasCivilesDataTable")
+    .dataTable({
+      aProcessing: true,
+      aServerSide: true,
+      dom: "Bfrtip",
+      searching: true,
+      lengthChange: false,
+      colReorder: true,
+      buttons: ["copyHtml5", "excelHtml5", "csvHtml5", "pdfHtml5"],
+      ajax: {
+        url: "../../controller/RecursosMaterialesProyectosObrasCivilesController.php?op=listado_recursos_materiales_proyectos_obras_civiles_clientes_por_proyecto_obra_civil_ID",
+        type: "post",
+        dataType: "json",
+        data: { proyectoObraCivilID: proyectoObraCivilID },
+        error: function (e) {
+          console.log(e.responseText);
+        },
+      },
+      ordering: false,
+      bDestroy: true,
+      responsive: true,
+      bInfo: true,
+      iDisplayLength: 10,
+      autoWidth: false,
+      language: {
+        sProcessing: "Procesando...",
+        sLengthMenu: "Mostrar _MENU_ registros",
+        sZeroRecords: "No se encontraron resultados",
+        sEmptyTable: "Ningun dato disponible en esta tabla",
+        sInfo: "Mostrando un total de _TOTAL_ registros",
+        sInfoEmpty: "Mostrando un total de 0 registros",
+        sInfoFiltered: "(filtrado de un total de _MAX_ registros)",
+        sInfoPostFix: "",
+        sSearch: "Buscar:",
+        sUrl: "",
+        sInfoThousands: ",",
+        sLoadingRecords: "Cargando....",
+        oPaginate: {
+          sFirst: "Primero",
+          sLast: "Ultimo",
+          sNext: "Siguiente",
+          sPrevious: "Anterior",
+        },
+        oAria: {
+          sSortAscending:
+            ": Activar para ordenar la columna de manera ascendente",
+          sSortDescending:
+            ": Activar para ordenar la columna de manera descendente",
+        },
+      },
+    })
+    .DataTable();
+};
+
 (function () {
   const proyectoObraCivilID = getParams("proyectoObraCivilID");
   const solicitudProyectoID = getParams("solicitudProyectoID");
@@ -280,9 +337,9 @@ const obtenerListadoActividadesProyectosObrasCivilesClientesDataTable = (
     proyectoObraCivilID
   );
 
-  //   obtenerListadoRecursosMaterialesProyectosObrasCivilesDataTable(
-  //     proyectoObraCivilID
-  //   );
+  obtenerListadoRecursosMaterialesProyectosObrasCivilesClientesDataTable(
+    proyectoObraCivilID
+  );
 
   //   obtenerListadoRecursosManosObrasProyectosObrasCivilesDataTable(
   //     proyectoObraCivilID
